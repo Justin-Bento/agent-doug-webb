89b33a0c6e4c035354f26def797946a01d6e7f70
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-07-30 21:13:37 -0400

    Created a list of links to navigate between pages within the interface.

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,8 +1,28 @@
+import { Button } from "@/components/ui/button";
+
 export default function Navigation() {
   return (
     <header className="overflow-hidden shadow">
       <div className="container mx-auto px-4 py-5 sm:p-6">
-        <p>I am the start of the website.</p>
+        <nav className="">
+          <ul className="flex items-center justify-end list-none">
+            <li>
+              <a href="#"><Button variant="ghost" size="sm">Link #1</Button></a>
+            </li>
+            <li>
+              <a href="#"><Button variant="ghost" size="sm">Link #2</Button></a>
+            </li>
+            <li>
+              <a href="#"><Button variant="ghost" size="sm">Link #3</Button></a>
+            </li>
+            <li>
+               <a href="#"><Button variant="ghost" size="sm">Link #3</Button></a>
+            </li>
+            <li>
+               <a href="#"><Button variant="ghost" size="sm">Link #3</Button></a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </header>
   );
