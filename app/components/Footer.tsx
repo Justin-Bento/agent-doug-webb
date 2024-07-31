be910c28befbe17aaaf5955c5f35dbaf44cee6d0
Justin Bento <bento.a.justin@gmail.com>
2024-07-30 20:47:47 -0400

    Simulated navigation component with boiler code.

---
+++ app/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>I am the end of the website.</p>
+    </footer>
+  )
+}
---
+++ app/components/Navigation.tsx
@@ -0,0 +1,8 @@
+
+export default function Navigation() {
+  return (
+    <header>
+        <p>This is the start of the website.</p>
+    </header>
+  )
+}
