786a32bdc356c1e3e13eab37a3a5b2662fe4914e
Justin Bento <bento.a.justin@gmail.com>
2024-07-30 20:48:04 -0400

    Imported components to the home page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
 
 export default function page() {
   return (
     <>
+    <Navigation />
       <main>
         <h1>Hello World</h1>
       </main>
+    <Footer/>
     </>
   );
 }
