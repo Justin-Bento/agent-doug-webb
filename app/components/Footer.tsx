ecbe07866e3e2bce449cfa2002d8abfce24cc17b
Justin Bento <bento.a.justin@gmail.com>
2024-07-30 20:49:30 -0400

    added a black background with overflow. So content dosen't overlap the container.

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer>
+    <footer className="overflow-hidden bg-black">
       <p>I am the end of the website.</p>
     </footer>
   )
