33991bbb5995135501b45d5d8a0117ee7c9ad48d
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-07-30 20:59:19 -0400

    Fixed typo from end to start. :)

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -2,7 +2,7 @@ export default function Navigation() {
   return (
     <header className="overflow-hidden shadow">
       <div className="container mx-auto px-4 py-5 sm:p-6">
-        <p>I am the end of the website.</p>
+        <p>I am the start of the website.</p>
       </div>
     </header>
   );
