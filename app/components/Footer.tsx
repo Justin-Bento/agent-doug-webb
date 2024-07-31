06b5e7cb96a50c09ab2b52736fbb1dd053f53a8c
Justin Bento <bento.a.justin@gmail.com>
2024-07-30 20:51:54 -0400

    Changed colour of the text to improve legibility.

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="overflow-hidden bg-black">
       <div className="container mx-auto px-4 py-5 sm:p-6">
-        <p>I am the end of the website.</p>
+        <p className="text-white">I am the end of the website.</p>
       </div>
     </footer>
   );
