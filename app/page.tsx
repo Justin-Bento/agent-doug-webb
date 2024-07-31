e4ebd11e6e99d66be07170ceb3f5521a949e4e01
Justin Bento <bento.a.justin@gmail.com>
2024-07-30 20:53:09 -0400

    Applied wrapper to main element  to give breathing room between elements.

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function page() {
   return (
     <>
     <Navigation />
-      <main>
+      <main className="container mx-auto min-h-dvh px-4 py-5 sm:p-6">
         <h1>Hello World</h1>
       </main>
     <Footer/>
