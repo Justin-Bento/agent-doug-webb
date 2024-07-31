8b173dce5e74de7fdbd7778a707cf4df037d769d
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-07-30 21:54:18 -0400

    Created all the pages for the application.

---
+++ app/moresuftt.tsx
@@ -0,0 +1,8 @@
+export default function MoreStuff() {
+    return (
+      <>
+        <h1>Hello, More Stuff Page!</h1>
+      </>
+    );
+  }
+  
---
+++ app/real-eastate-trasnaction.tsx
@@ -0,0 +1,8 @@
+export default function RealEstateTransaction() {
+    return (
+      <>
+        <h1>Hello, Real-Estate Transaction Page!</h1>
+      </>
+    );
+  }
+  
