4a2a492f4d435fbfbe30474b3b2547256d6eca5f
Justin Bento <bento.a.justin@gmail.com>
2024-07-30 20:51:20 -0400

    Added Constrained container with padded content

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="overflow-hidden bg-black">
-      <div className="px-4 py-5 sm:p-6">
+      <div className="container mx-auto px-4 py-5 sm:p-6">
         <p>I am the end of the website.</p>
       </div>
     </footer>
