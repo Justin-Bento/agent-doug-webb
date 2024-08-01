371014d51151c5f080f987ad0ebab07047ea95f4
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-08-01 10:02:56 -0400

    Imported base layouts for the interface

--- app/listings/page.tsx
+++ app/listings/page.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import Heading from '../components/Heading'
+import React from "react";
+import Heading from "../components/Heading";
+import Navigation from "../components/Navigation";
+import Footer from "../components/Footer";
 
 export default function page() {
   return (
     <div>
-      <Heading heading="Hello World" supporting="the quick brown fox jumps over the lazy dog." />
+      <Navigation />
+      <main className="container mx-auto min-h-dvh px-4 py-5 sm:p-6">
+        <Heading
+          heading="Hello world, from listing page."
+          supporting="the quick brown fox jumps over the lazy dog."
+        />
+      </main>
+      <Footer />
     </div>
-  )
+  );
 }
