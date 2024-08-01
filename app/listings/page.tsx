43d3daa44d82a7e456c343db071ca938f26e0957
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-08-01 10:16:38 -0400

    included a message variable to store long supporting text.

--- app/listings/page.tsx
+++ app/listings/page.tsx
@@ -4,13 +4,14 @@ import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
 
 export default function page() {
+  let message: string = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto doloribus aspernatur tenetur officiis quasi numquam eos qui praesentium, at placeat eligendi reiciendis dicta similique sapiente porro quis tempore voluptatibus aliquam?"
   return (
     <div>
       <Navigation />
       <main className="container mx-auto min-h-dvh px-4 py-5 sm:p-6">
         <Heading
           heading="Hello world, from listing page."
-          supporting="the quick brown fox jumps over the lazy dog."
+          supporting={message}
         />
       </main>
       <Footer />
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
+import Heading from "./components/Heading";
 
 export default function page() {
+  let message: string = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto doloribus aspernatur tenetur officiis quasi numquam eos qui praesentium, at placeat eligendi reiciendis dicta similique sapiente porro quis tempore voluptatibus aliquam?"
   return (
     <>
     <Navigation />
       <main className="container mx-auto min-h-dvh px-4 py-5 sm:p-6">
-        <h1>Home</h1>
+        <Heading heading="Hello, Landing Page!" supporting={message} />
       </main>
     <Footer/>
     </>
