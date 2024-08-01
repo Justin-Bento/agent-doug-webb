8b18932c8e0ec14f24f8e32fd1caeb2c1aba10a0
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-08-01 10:04:13 -0400

    Updated heading and supprting styles.

--- app/components/Heading.tsx
+++ app/components/Heading.tsx
@@ -5,8 +5,8 @@ interface headingTypes {
 export default function Heading({heading, supporting}:headingTypes) {
   return (
     <div>
-        <h1 className="">{heading}</h1>
-        <p className="">{supporting}</p>
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">{heading}</h1>
+        <p className="leading-7 text-lg">{supporting}</p>
     </div>
   )
 }
