79c0eafd72d99ac06a74d99c5adb9fe06cb62ffa
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-07-31 20:50:22 -0400

    renamed file to <pages>to defined a special file of a route segment.

--- app/real-estate-process/page.tsx
+++ app/real-estate-process/page.tsx
@@ -1,7 +1,9 @@
 export default function RealEstateTransaction() {
     return (
       <>
+        <main className="">
         <h1>Hello, Real-Estate Transaction Page!</h1>
+        </main>
       </>
     );
   }
