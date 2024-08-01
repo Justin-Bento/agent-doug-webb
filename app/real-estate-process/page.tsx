3854bc4ed22cd1c6896f3f39a94bb24f65e47e7b
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-07-31 20:52:38 -0400

    included container wrapper

--- app/real-estate-process/page.tsx
+++ app/real-estate-process/page.tsx
@@ -5,7 +5,7 @@ export default function RealEstateTransaction() {
   return (
     <>
       <Navigation />
-      <main className="">
+      <main className="container mx-auto min-h-dvh px-4 py-5 sm:p-6">
         <h1>Hello, Real-Estate Transaction Page!</h1>
       </main>
       <Footer />
