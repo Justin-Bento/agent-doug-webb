6b37f10140be97a4b41574e85418b89f6113e298
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-08-01 10:13:36 -0400

    moved strings into it's own variable

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,12 +3,13 @@ import Heading from "../components/Heading";
 import Navigation from "../components/Navigation";
 
 export default function Contact() {
+  let title: string = "Hello, Contact Page!"
   let message: string = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto doloribus aspernatur tenetur officiis quasi numquam eos qui praesentium, at placeat eligendi reiciendis dicta similique sapiente porro quis tempore voluptatibus aliquam?"
   return (
     <>
       <Navigation />
       <main className="container mx-auto min-h-dvh px-4 py-5 sm:p-6">
-        <Heading heading="Hello, Contact Page!" supporting={message} />
+        <Heading heading={title} supporting={message} />
       </main>
       <Footer />
     </>
