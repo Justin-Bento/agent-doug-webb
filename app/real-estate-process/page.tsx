8f580a18d6f8fd2a7da7c1c924cb89d09993a9b5
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-07-31 21:04:51 -0400

    Created a base layout for content

--- app/real-estate-process/page.tsx
+++ app/real-estate-process/page.tsx
@@ -6,7 +6,14 @@ export default function RealEstateTransaction() {
     <>
       <Navigation />
       <main className="container mx-auto min-h-dvh px-4 py-5 sm:p-6">
-        <h1>Hello, Real-Estate Transaction Page!</h1>
+        <div className="flex min-h-dvh gap-9">
+          <div className="bg-gray-200 w-64 flex-none">
+            <p className="">Side Navigation</p>
+          </div>
+          <div className="w-full bg-gray-200">
+            <h1>Hello, Real-Estate Transaction Page!</h1>
+          </div>
+        </div>
       </main>
       <Footer />
     </>
