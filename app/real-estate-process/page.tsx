774b1e247ee8a651af14f4720cc9116375ab7756
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-07-31 21:32:15 -0400

    Transfered SideNavigation function into it's own component.
    Doing this enabled me to seperated code between page sections.

--- app/real-estate-process/page.tsx
+++ app/real-estate-process/page.tsx
@@ -1,6 +1,6 @@
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
-import { Button } from "@/components/ui/button";
+import SideNavigation from "../components/SideNavigation";
 
 export default function RealEstateTransaction() {
   return (
@@ -15,73 +15,6 @@ export default function RealEstateTransaction() {
         </div>
       </main>
       <Footer />
-    </>
-  );
-}
-
-const real_estate_stages = {
-  deciding_to_buy: [
-    "Sell before you buy",
-    "How Much can you afford?",
-    "Selecting an Buying Agent",
-    "The Buyers RE. Contract",
-    "Searching Properties",
-    "Visiting Properties",
-    "Making an Offer",
-    "Negotiating the Offer",
-    "Making The Offer Firm",
-    "Lawyers & Closing B.",
-    "Packing and Moving",
-  ],
-  deciding_to_sell: [
-    "Researching Value",
-    "Prepping Property",
-    "Selecting an Selling Agent",
-    "The Listing Contract",
-    "Staging",
-    "Showing",
-    "Negotatiing",
-    "Removing Conditions",
-    "Lawyers & Closing Selling",
-    "Packing & Moving",
-  ],
-};
-
-function SideNavigation() {
-  return (
-    <>
-      <div className="w-64 flex-none">
-        <div className="">
-          <p className="font-bold">Deciding to Buy</p>
-          <hr className="mt-2" />
-          <ul className="mt-4">
-            {real_estate_stages.deciding_to_buy.map((buy_steps, index) => {
-              return (
-                <li key={index}>
-                  <Button variant="link" className="w-full justify-start">
-                    {buy_steps}
-                  </Button>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-        <div className="mt-8">
-          <p className="font-bold">Deciding to Sell</p>
-          <hr className="mt-2" />
-          <ul className="mt-4">
-            {real_estate_stages.deciding_to_sell.map((sell_steps, index) => {
-              return (
-                <li key={index}>
-                  <Button variant="link" className="w-full justify-start">
-                    {sell_steps}
-                  </Button>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      </div>
     </>
   );
 }
