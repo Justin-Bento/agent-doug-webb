38cd0cd4ff6f839fd75cb5ac2d0227371f853b5e
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-08-01 10:00:56 -0400

    Created a new page called listings to showcase properties.

---
+++ app/listings/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Heading from '../components/Heading'
+
+export default function page() {
+  return (
+    <div>
+      <Heading heading="Hello World" supporting="the quick brown fox jumps over the lazy dog." />
+    </div>
+  )
+}
