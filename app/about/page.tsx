358e82ac21443b1f0e28cd5c46e172673904f50f
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-08-01 23:26:12 -0400

    Fixed spacing and alignment

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Footer from "../components/Footer";
 import Navigation from "../components/Navigation";
 import Image from "next/image";
@@ -9,11 +10,11 @@ export default function about() {
       <Navigation />
       <main className="min-h-dvh">
         <section className="container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8 my-16">
-          <div className="w-full  rounded-lg bg-gray-200">
-            <Image src="" alt="" className="aspect-video" />
+          <div className="w-full rounded-lg bg-gray-200">
+            <Image src="" alt="" className="aspect-square" />
           </div>
           <div className="w-full h-24">
-            <h1 className="text-4xl font-bold tracking-tight mb-4">
+            <h1 className="text-4xl font-bold tracking-tight mt-6 mb-4">
               About, Douglas J. Webb!
             </h1>
             <div className="space-y-4">
@@ -40,7 +41,18 @@ export default function about() {
                 <li>Real Estate investor since 1985</li>
                 <li>70% Repeat Clients and Referral</li>
               </ul>
+              <p>
+                Today, I&#39;m the founder of Planetaria, where we&#39;re
+                working on civilian space suits and manned shuttle kits you can
+                assemble at home so that the next generation of kids really can
+                make it to orbit — from the comfort of their own backyards.
+              </p>
             </div>
+            <hr className="border mt-8" />
+            <span className="flex items-center gap-1 mt-6">
+              <p className="leading-7">don&#39;t hesitate to email me:</p>
+              <Button variant="link">email@agentwebb.com</Button>
+            </span>
           </div>
         </section>
       </main>
