2d0d496ba0af2ef92d47ab8f2dcf4f6047dd8009
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-08-01 23:44:07 -0400

    included a section asking for people's emails

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,8 +8,8 @@ export default function about() {
   return (
     <>
       <Navigation />
-      <main className="min-h-dvh">
-        <section className="container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8 my-16">
+      <main className="">
+        <section className="container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8 my-24">
           <div className="w-full rounded-lg bg-gray-200">
             <Image src="" alt="" className="aspect-square" />
           </div>
@@ -53,6 +53,47 @@ export default function about() {
               <p className="leading-7">don&#39;t hesitate to email me:</p>
               <Button variant="link">email@agentwebb.com</Button>
             </span>
+          </div>
+        </section>
+        <section className="bg-indigo-50 py-28">
+          <div className="container mx-auto grid grid-cols-1 gap-10 px-6 lg:grid-cols-12 lg:gap-8 lg:px-8">
+            <div className="max-w-xl text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl lg:col-span-7">
+              <h2 className="inline sm:block lg:inline xl:block">
+                Want product news and updates?
+              </h2>
+              <p className="inline sm:block lg:inline xl:block">
+                Sign up for our newsletter.
+              </p>
+            </div>
+            <form className="w-full max-w-md lg:col-span-5 lg:pt-2">
+              <div className="flex gap-x-4">
+                <input
+                  id="email-address"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="min-w-0 flex-auto rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  placeholder="Enter your email"
+                />
+                <button
+                  type="submit"
+                  className="flex-none rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Subscribe
+                </button>
+              </div>
+              <p className="mt-4 text-sm leading-6 text-gray-900">
+                We care about your data. Read our{" "}
+                <a
+                  href="#"
+                  className="font-semibold text-indigo-600 hover:text-indigo-500"
+                >
+                  privacy&nbsp;policy
+                </a>
+                .
+              </p>
+            </form>
           </div>
         </section>
       </main>
