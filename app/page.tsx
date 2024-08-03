845028ebf65d03b1073ae2bd1d0b4c464bd035c3
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-08-03 16:31:24 -0400

    Styled headings for consistency

--- app/page.tsx
+++ app/page.tsx
@@ -14,32 +14,32 @@ export default function page() {
         </section>
         <section className="">
           <div className="container mx-auto px-4 py-5 sm:p-6">
-            <h1 className="">Welcome to section 1</h1>
+            <h1 className="text-3xl capitalize font-bold tracking-tight dark:text-white">Welcome to section 1</h1>
           </div>
         </section>
         <section className="">
           <div className="container mx-auto px-4 py-5 sm:p-6">
-            <h2 className="">Welcome to section 2</h2>
+            <h2 className="text-3xl capitalize font-bold tracking-tight dark:text-white">Welcome to section 2</h2>
           </div>
         </section>
         <section className="">
           <div className="container mx-auto px-4 py-5 sm:p-6">
-            <h3 className="">Welcome to section 3</h3>
+            <h3 className="text-3xl capitalize font-bold tracking-tight dark:text-white">Welcome to section 3</h3>
           </div>
         </section>
         <section className="">
           <div className="container mx-auto px-4 py-5 sm:p-6">
-            <h4 className="">Welcome to section 4</h4>
+            <h4 className="text-3xl capitalize font-bold tracking-tight dark:text-white">Welcome to section 4</h4>
           </div>
         </section>
         <section className="">
           <div className="container mx-auto px-4 py-5 sm:p-6">
-            <h5 className="">Welcome to section 5</h5>
+            <h5 className="text-3xl capitalize font-bold tracking-tight dark:text-white">Welcome to section 5</h5>
           </div>
         </section>
         <section className="">
           <div className="container mx-auto px-4 py-5 sm:p-6">
-            <h6 className="">Welcome to section 6</h6>
+            <h6 className="text-3xl capitalize font-bold tracking-tight dark:text-white">Welcome to section 6</h6>
           </div>
         </section>
       </main>
