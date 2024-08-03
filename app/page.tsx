f2e1cef6160669420234dc496eb3ac864270958b
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-08-03 17:13:05 -0400

    Replaced link icon with next-link

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
 import { Button } from "@/components/ui/button";
-import { Link } from "lucide-react";
+import Link  from "next/link";
 
 export default function page() {
   return (
