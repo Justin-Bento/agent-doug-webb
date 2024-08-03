85889a38438a3649e1f45a3eae5c243eba9c5792
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-08-03 16:29:56 -0400

    Added 6 section cards with it's own container using emmet.

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,39 @@ export default function page() {
   return (
     <>
     <Navigation />
-      <main className="min-h-dvh">
+      <main className="min-h-dvh space-y-20 my-20">
         <section className="container mx-auto px-4 py-5 sm:p-6">
           <Heading heading="Hello, Landing Page!" supporting={message} />
+        </section>
+        <section className="">
+          <div className="container mx-auto px-4 py-5 sm:p-6">
+            <h1 className="">Welcome to section 1</h1>
+          </div>
+        </section>
+        <section className="">
+          <div className="container mx-auto px-4 py-5 sm:p-6">
+            <h2 className="">Welcome to section 2</h2>
+          </div>
+        </section>
+        <section className="">
+          <div className="container mx-auto px-4 py-5 sm:p-6">
+            <h3 className="">Welcome to section 3</h3>
+          </div>
+        </section>
+        <section className="">
+          <div className="container mx-auto px-4 py-5 sm:p-6">
+            <h4 className="">Welcome to section 4</h4>
+          </div>
+        </section>
+        <section className="">
+          <div className="container mx-auto px-4 py-5 sm:p-6">
+            <h5 className="">Welcome to section 5</h5>
+          </div>
+        </section>
+        <section className="">
+          <div className="container mx-auto px-4 py-5 sm:p-6">
+            <h6 className="">Welcome to section 6</h6>
+          </div>
         </section>
       </main>
     <Footer/>
