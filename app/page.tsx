6a43e544e2ee46e375c0eaf13324044918d09740
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-08-03 16:36:19 -0400

    Removed old section

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,16 @@
 import React from "react";
 import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
-import Heading from "./components/Heading";
 
 export default function page() {
-  let message: string = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto doloribus aspernatur tenetur officiis quasi numquam eos qui praesentium, at placeat eligendi reiciendis dicta similique sapiente porro quis tempore voluptatibus aliquam?"
   return (
     <>
     <Navigation />
       <main className="min-h-dvh space-y-20 my-20">
-        <section className="container mx-auto px-4 py-5 sm:p-6">
-          <Heading heading="Hello, Landing Page!" supporting={message} />
-        </section>
         <section className="">
           <div className="container mx-auto px-4 py-5 sm:p-6">
             <h1 className="text-3xl capitalize font-bold tracking-tight dark:text-white">Welcome to section 1</h1>
+            <p className=""></p>
           </div>
         </section>
         <section className="">
