0d006a820045bed2d4c3557224361487c5d7e610
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-08-04 13:43:40 -0400

    Imported a blog header component to keep consistent visuals across the interface

---
+++ components/ui/BlogHeader.tsx
@@ -0,0 +1,18 @@
+interface BlogHeaderProps {
+  Title: string;
+  Description: string;
+}
+export default function BlogHeader({ Title, Description }: BlogHeaderProps) {
+  return (
+    <section className="text-balance">
+      <h1 className="scroll-m-20 text-4xl font-bold capitalize tracking-tight">
+        {Title}
+      </h1>
+      <p className="leading-6 line-clamp-2 mt-2 mb-6">
+        {Description ||
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe nesciunt optio cupiditate. Doloribus, dolor. Quod, fugiat, alias voluptatibus quo magnam debitis illo eos suscipit velit, blanditiis aperiam. Consectetur, est ipsa qui accusantium sequi rem accusamus vero illum quae ad corrupti."}
+      </p>
+      <div className="border-b border-gray-500"></div>
+    </section>
+  );
+}
