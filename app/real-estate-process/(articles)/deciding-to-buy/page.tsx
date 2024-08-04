868d38613ea6be08b94d70e5acf73bf6040cdefc
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-08-04 17:30:29 -0400

    Deleted navigation elements from the article layout inside the main interface.
    - Renamed RealEstateTransaction to DecidingToBuyArticles for better transparency.

--- app/real-estate-process/(articles)/deciding-to-buy/page.tsx
+++ app/real-estate-process/(articles)/deciding-to-buy/page.tsx
@@ -1,28 +1,9 @@
-import SideNavigation from "../../../../components/SideNavigation";
-import { Button } from "@/components/ui/button";
-import DecidingToBuy from "./deciding-to-buy.mdx";
+import Step1 from "./step1.mdx";
 
-export default function RealEstateTransaction() {
+export default function DecidingToBuyArticles() {
   return (
     <>
-      <section className="grid grid-cols-12 my-16">
-        <article className="col-span-full xl:col-span-7 xl:col-start-2 xl:max-w-[100ch]">
-          <DecidingToBuy />
-          <div className="flex items-center justify-between mt-8 border-t-2 pt-8">
-            <Button size="sm" variant="outline">
-              Previous Article
-            </Button>
-            <Button size="sm" variant="outline">
-              Next Article
-            </Button>
-          </div>
-        </article>
-        <aside className="col-span-3 col-start-10 justify-self-end hidden xl:block">
-          <div className="sticky top-12 pb-20">
-            <SideNavigation stage="buy" />
-          </div>
-        </aside>
-      </section>
+      <Step1 />
     </>
   );
 }
