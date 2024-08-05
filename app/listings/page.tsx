86eeb9b90c3f3acae17ebc47e849dcf0e9e13b59
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-08-05 10:57:35 -0400

    Created the filter section that will aims to filter sections based off the listing

--- app/listings/page.tsx
+++ app/listings/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Heading from "../../components/Heading";
 import Navigation from "../../components/Navigation";
 import Footer from "../../components/Footer";
+import { Button } from "@/components/ui/button";
 
 export default function page() {
   let message: string = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto doloribus aspernatur tenetur officiis quasi numquam eos qui praesentium, at placeat eligendi reiciendis dicta similique sapiente porro quis tempore voluptatibus aliquam?"
@@ -13,6 +14,20 @@ export default function page() {
           heading="Hello world, from listing page."
           supporting={message}
         />
+        <section id="filter" className="bg-gray-300 p-2 my-4 rounded-lg ring-1 ring-gray-400">
+          <ul className="flex items-center gap-4 list-none">
+            <li>
+              <Button variant="ghost" size="sm">For Sale</Button>
+            </li>
+            <li>
+              <Button variant="ghost" size="sm">Rent</Button>
+            </li>
+            <li>
+              <Button variant="ghost" size="sm">For Purchase</Button>
+            </li>
+          </ul>
+        </section>
+        <section id="real-estate-properties"></section>
       </main>
       <Footer />
     </div>
