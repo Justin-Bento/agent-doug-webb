bbd7eda47fbacf667f141a79b60f8ef21f7ca750
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-08-05 11:11:52 -0400

    Created a property card so show listings

--- app/listings/page.tsx
+++ app/listings/page.tsx
@@ -3,6 +3,7 @@ import Heading from "../../components/Heading";
 import Navigation from "../../components/Navigation";
 import Footer from "../../components/Footer";
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 
 export default function page() {
   let message: string = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto doloribus aspernatur tenetur officiis quasi numquam eos qui praesentium, at placeat eligendi reiciendis dicta similique sapiente porro quis tempore voluptatibus aliquam?"
@@ -27,9 +28,35 @@ export default function page() {
             </li>
           </ul>
         </section>
-        <section id="real-estate-properties"></section>
+        <section id="real-estate-properties">
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 mt-14">
+            <CardWFooter className="bg-gray-300" />
+            <CardWFooter className="bg-red-300" />
+            <CardWFooter className="bg-blue-300" />
+            <CardWFooter className="bg-gray-300" />
+            <CardWFooter className="bg-blue-300" />
+            <CardWFooter className="bg-red-300" />
+          </div>
+        </section>
       </main>
       <Footer />
     </div>
   );
+}
+
+function CardWFooter({className}: any) {
+  return (
+    <div className="divide-y divide-gray-900 overflow-hidden rounded-lg bg-white ring-1 ring-gray-900">
+      <div className="rounded-lg">
+        {/* Content goes here */}
+        <div className={cn("bg-gray-300 w-full aspect-video", className)}></div>
+      </div>
+      <div className="px-4 py-4 sm:p-6 space-y-1">
+        {/* Content goes here */}
+        <h3 className="text-lg tracking-tight font-bold dark:text-white">Whispering Pines Estate</h3>
+        <p className="line-clamp-3">Nestled among tall, serene pines, Whispering Pines Estate offers a tranquil retreat with modern amenities and breathtaking forest views. Ideal for nature lovers, this charming property combines rustic elegance with contemporary comfort.</p>
+        <Button variant="secondary" size="sm" className="flex items-center gap-1">Hello World <span aria-hidden="true">→</span></Button>
+      </div>
+    </div>
+  )
 }
