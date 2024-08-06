fe0b525ecb8d1cad33d775b069b5416f0d19c6fd
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-08-05 22:04:17 -0400

    Added Company Culture section to main webpage.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -11,7 +11,7 @@ export default function about() {
       <main className="min-h-dvh my-24 space-y-24">
         <section className="container mx-auto">
           <div className="mx-auto max-w-2xl lg:max-w-none">
-            <div style={{opacity: 1, transform: 'none'}}>
+            <div style={{ opacity: 1, transform: "none" }}>
               <h1>
                 <span className="mb-3 block font-display text-base font-semibold text-black">
                   About us
@@ -84,11 +84,82 @@ export default function about() {
             </div>
           </dl>
         </section>
+        <section id="company-values" className="container mx-auto bg-black rounded-xl">
+          <div className="mt-24 rounded-4xl py-24 lg:py-32 ">
+            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+              <div className="mx-auto max-w-2xl lg:max-w-none">
+                <div className="max-w-2xl" style={{opacity: 1, transform: 'none'}}>
+                  <h2>
+                    <span className="mb-6 block font-display text-base font-semibold text-white">
+                      Our culture
+                    </span>
+                    <span className="sr-only"> - </span>
+                    <span className="block font-display tracking-tight [text-wrap:balance] text-4xl font-medium sm:text-5xl text-white">
+                      Balance your passion with your passion for life.
+                    </span>
+                  </h2>
+                  <div className="mt-6 text-xl text-white">
+                    <p>
+                      We are a group of like-minded people who share the same
+                      core values.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="mx-auto max-w-7xl px-6 lg:px-8 mt-16">
+              <div className="mx-auto max-w-2xl lg:max-w-none">
+                <div>
+                  <ul
+                    role="list"
+                    className="grid grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-3"
+                  >
+                    <li className="text-base text-white before:bg-white after:bg-white/10">
+                      <div style={{opacity: 1, transform: 'none'}}>
+                        <div className="pl-8 relative before:absolute after:absolute before:bg-white after:bg-white/10 before:left-0 before:top-0 before:h-6 before:w-px after:bottom-0 after:left-0 after:top-8 after:w-px">
+                          <strong className="font-semibold text-white mr-1">
+                            Loyalty.
+                          </strong>
+                          Our team has been with us since the beginning because
+                          none of them are allowed to have LinkedIn profiles.
+                        </div>
+                      </div>
+                    </li>
+                    <li className="text-base text-white before:bg-white after:bg-white/10">
+                      <div style={{opacity: 1, transform: 'none'}}>
+                        <div className="pl-8 relative before:absolute after:absolute before:bg-white after:bg-white/10 before:left-0 before:top-0 before:h-6 before:w-px after:bottom-0 after:left-0 after:top-8 after:w-px">
+                          <strong className="font-semibold text-white mr-1">
+                            Trust.
+                          </strong>
+                          We don’t care when our team works just as long as they
+                          are working every waking second.
+                        </div>
+                      </div>
+                    </li>
+                    <li className="text-base text-white before:bg-white after:bg-white/10">
+                      <div style={{opacity: 1, transform: 'none'}}>
+                        <div className="pl-8 relative before:absolute after:absolute before:bg-white after:bg-white/10 before:left-0 before:top-0 before:h-6 before:w-px after:bottom-0 after:left-0 after:top-8 after:w-px">
+                          <strong className="font-semibold text-white mr-1">
+                            Compassion.
+                          </strong>
+                          You never know what someone is going through at home
+                          and we make sure to never find out.
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
         <section className="container mx-auto">
           <div className="grid grid-cols-2 gap-8 items-start">
             <div className="bg-gray-400 aspect-square rounded-xl"></div>
             <div className="mt-4 max-w-4xl space-y-6 text-base">
-             <h2 className="text-4xl font-bold capitalize dark:text-white">Douglas J. Webb</h2>
+              <h2 className="text-4xl font-bold capitalize dark:text-white">
+                Douglas J. Webb
+              </h2>
               <p>
                 Agent Webb is more than just one agent — it is a comprehensive
                 platform for Real Estate Transactions. This means people can
