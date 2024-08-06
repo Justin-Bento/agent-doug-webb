097f381d4dddb2c20915d0c501a61c7fa1479519
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-08-05 21:30:50 -0400

    created a stats page showcasing his experience and reputation.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,8 +8,8 @@ export default function about() {
   return (
     <>
       <Navigation />
-      <main className="min-h-dvh">
-        <section className="container mx-auto my-24">
+      <main className="min-h-dvh my-24 space-y-24">
+        <section className="container mx-auto">
           <div className="text-balance mb-6">
             <div className="flex flex-col-reverse">
               <h1 className="text-5xl tracking-tight font-extrabold capitalize dark:text-white">
@@ -26,34 +26,76 @@ export default function about() {
             <div className="bg-gray-400 aspect-square"></div>
             <div className="mt-4 max-w-4xl space-y-6 text-base">
               <p>
-                Doug got involved in residential investment real estate in 1986
-                which ultimately led to a full time career in real estate sales
-                in 1997 with RE/Max. To provide his clients with the latitude of
-                service which he and most of his clients sought, Doug moved to
-                Right At Home Realty. The business generated through the
-                referral of his satisfied clients along with their letters and
-                thank-you cards is something he is very proud of.
+                Studio was started by Douglas J. Webb who noticed that people
+                are afraid of residential investment as there is a lot of
+                secrecy behind home ownership. Since the beginning, he has been
+                committed to doing things differently by being transparent and
+                share his knowledge instead.
               </p>
-              <ul className="list-disc list-inside space-y-4">
-                <li>Certified Residential Specialist</li>
-                <li>Canadian Real Estate Association</li>
-                <li>Realtor® Sales Award</li>
-                <li>Serving: Clients since 1997</li>
-                <li>Member Toronto Real Estate Board</li>
-                <li>MLS - Multiple Listing Service.</li>
-                <li>Real Estate investor since 1985</li>
-                <li>Real Estate investor since 1985</li>
-                <li>70% Repeat Clients and Referral</li>
-              </ul>
               <p>
-                Today, I&#39;m the founder of Planetaria, where we&#39;re
-                working on civilian space suits and manned shuttle kits you can
-                assemble at home so that the next generation of kids really can
-                make it to orbit — from the comfort of their own backyards.
+                Agent Webb is more than just one agent — it is a comprehensive
+                platform for Real Estate Transactions. This means people can
+                access different stages of the process on the go. Whether you're
+                buying or selling a property, Agent Webb provides you with all
+                the tools and resources you need. From initial property searches
+                to finalizing the transaction, our platform offers step-by-step
+                guidance, expert advice, and real-time updates to ensure a
+                smooth and efficient experience.
               </p>
-              <Button>Hello World</Button>
+              <p>
+                With Agent Webb, you can easily navigate the complexities of the
+                real estate market, stay informed about important milestones,
+                and make well-informed decisions every step of the way.
+              </p>
+              <p>
+                With Agent Webb, you can easily navigate the complexities of the
+                real estate market, stay informed about important milestones,
+                and make well-informed decisions every step of the way.
+              </p>
+              <p>
+                With Agent Webb, you can easily navigate the complexities of the
+                real estate market, stay informed about important milestones,
+                and make well-informed decisions every step of the way.
+              </p>
             </div>
           </div>
+        </section>
+        <section className="container mx-auto">
+          <dl className="grid grid-cols-1 gap-10 sm:grid-cols-2 lg:auto-cols-fr lg:grid-flow-col lg:grid-cols-none">
+            <div
+              className="flex flex-col-reverse pl-8 relative before:absolute after:absolute before:bg-neutral-950 after:bg-neutral-950/10 before:left-0 before:top-0 before:h-6 before:w-px after:bottom-0 after:left-0 after:top-8 after:w-px"
+              style={{ opacity: 1, transform: "none" }}
+            >
+              <dt className="mt-2 text-base text-neutral-600">
+                Certified Real Estate investor
+              </dt>
+              <dd className="font-display text-3xl font-semibold text-neutral-950 sm:text-4xl">
+                1985
+              </dd>
+            </div>
+            <div
+              className="flex flex-col-reverse pl-8 relative before:absolute after:absolute before:bg-neutral-950 after:bg-neutral-950/10 before:left-0 before:top-0 before:h-6 before:w-px after:bottom-0 after:left-0 after:top-8 after:w-px"
+              style={{ opacity: 1, transform: "none" }}
+            >
+              <dt className="mt-2 text-base text-neutral-600">
+                Years of experience
+              </dt>
+              <dd className="font-display text-3xl font-semibold text-neutral-950 sm:text-4xl">
+                25+
+              </dd>
+            </div>
+            <div
+              className="flex flex-col-reverse pl-8 relative before:absolute after:absolute before:bg-neutral-950 after:bg-neutral-950/10 before:left-0 before:top-0 before:h-6 before:w-px after:bottom-0 after:left-0 after:top-8 after:w-px"
+              style={{ opacity: 1, transform: "none" }}
+            >
+              <dt className="mt-2 text-base text-neutral-600">
+                Satisfied Clients and Referral
+              </dt>
+              <dd className="font-display text-3xl font-semibold text-neutral-950 sm:text-4xl">
+                70%
+              </dd>
+            </div>
+          </dl>
         </section>
       </main>
       <Footer />
