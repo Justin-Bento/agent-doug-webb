714b43823bd0b6af772b94eefe00b8bdd7ae0689
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-08-06 19:10:20 -0400

    Included a list of links to navigate to pages within the ui

--- app/real-estate-process/page.tsx
+++ app/real-estate-process/page.tsx
@@ -1,6 +1,7 @@
+import React from "react";
 import Footer from "@/components/Footer";
 import Navigation from "@/components/Navigation";
-import React from "react";
+import Link from "next/link";
 
 export default function page() {
   return (
@@ -8,7 +9,29 @@ export default function page() {
       <Navigation />
       <main className="min-h-dvh">
         <section className="container mx-auto my-28">
-          <h1 className="text-4xl tracking-tight font-bold dark:text-white">Welcome, Real Estate Page.</h1>
+          <h1 className="text-4xl tracking-tight font-bold dark:text-white">Welcome, Real-Estate process main page.</h1>
+          <ul className="list-disc list-inside my-6 space-y-6">
+            <li className="">
+              <Link href="/real-estate-process/deciding-to-buy" className="hover:underline">
+                Deciding To Buy
+              </Link>
+            </li>
+            <li className="">
+              <Link href="/real-estate-process/deciding-to-sell" className="hover:underline">
+                Deciding To Sell
+              </Link>
+            </li>
+            <li className="">
+              <Link href="/real-estate-process/more-stuff" className="hover:underline">
+                More Stuff
+              </Link>
+            </li>
+            <li className="">
+              <Link href="/real-estate-process/additional-information" className="hover:underline">
+                Additional Information
+              </Link>
+            </li>
+          </ul>
         </section>
       </main>
       <Footer />
