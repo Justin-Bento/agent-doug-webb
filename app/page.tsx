10199f53611be357845cd7c2cd33858a494e968e
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-08-07 09:55:18 -0400

    Added a features section for staying with agent-webb

--- app/page.tsx
+++ app/page.tsx
@@ -184,7 +184,62 @@ export default function page() {
         </section>
         <section className="">
           <div className="container mx-auto px-4 py-5 sm:p-6">
-            <h5 className="text-4xl tracking-tight font-extrabold capitalize dark:text-white">Welcome to section 5</h5>
+            <div className="bg-white py-24 sm:py-32">
+              <div className="mx-auto max-w-2xl lg:mx-0">
+                <h5 className="text-4xl tracking-tight font-extrabold capitalize dark:text-white">
+                  Welcome to section 5
+                </h5>
+
+                <p className="mt-6 text-lg leading-8 text-gray-600">
+                  Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum cupiditate veritatis
+                  in accusamus quisquam.
+                </p>
+              </div>
+              <dl className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 text-base leading-7 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+                <div>
+                  <dt className="font-semibold text-gray-900">Push to deploy.</dt>
+                  <dd className="mt-1 text-gray-600">
+                    Aut illo quae. Ut et harum ea animi natus. Culpa maiores et sed sint et magnam exercitationem quia.
+                    Ullam voluptas nihil vitae dicta molestiae et. Aliquid velit porro vero.
+                  </dd>
+                </div>
+                <div>
+                  <dt className="font-semibold text-gray-900">SSL certificates.</dt>
+                  <dd className="mt-1 text-gray-600">
+                    Mollitia delectus a omnis. Quae velit aliquid. Qui nulla maxime adipisci illo id molestiae. Cumque
+                    cum ut minus rerum architecto magnam consequatur. Quia quaerat minima.
+                  </dd>
+                </div>
+                <div>
+                  <dt className="font-semibold text-gray-900">Simple queues.</dt>
+                  <dd className="mt-1 text-gray-600">
+                    Aut repellendus et officiis dolor possimus. Deserunt velit quasi sunt fuga error labore quia ipsum.
+                    Commodi autem voluptatem nam. Quos voluptatem totam.
+                  </dd>
+                </div>
+                <div>
+                  <dt className="font-semibold text-gray-900">Advanced security.</dt>
+                  <dd className="mt-1 text-gray-600">
+                    Magnam provident veritatis odit. Vitae eligendi repellat non. Eum fugit impedit veritatis ducimus.
+                    Non qui aspernatur laudantium modi. Praesentium rerum error deserunt harum.
+                  </dd>
+                </div>
+                <div>
+                  <dt className="font-semibold text-gray-900">Powerful API.</dt>
+                  <dd className="mt-1 text-gray-600">
+                    Sit minus expedita quam in ullam molestiae dignissimos in harum. Tenetur dolorem iure. Non nesciunt
+                    dolorem veniam necessitatibus laboriosam voluptas perspiciatis error.
+                  </dd>
+                </div>
+                <div>
+                  <dt className="font-semibold text-gray-900">Database backups.</dt>
+                  <dd className="mt-1 text-gray-600">
+                    Ipsa in earum deserunt aut. Quos minus aut animi et soluta. Ipsum dicta ut quia eius. Possimus
+                    reprehenderit iste aspernatur ut est velit consequatur distinctio.
+                  </dd>
+                </div>
+              </dl>
+            </div>
           </div>
         </section>
         <section className="">
