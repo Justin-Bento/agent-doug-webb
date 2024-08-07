f097edda591412e73a423bd1d2c95d871254b7d0
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-08-07 11:31:05 -0400

    Polished the navigation

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -4,58 +4,171 @@ import Link from "next/link";
 
 export default function Navigation() {
   return (
-    <header className="overflow-hidden border-b border-black/10 shadow shadow-black/10 sticky top-0 left-0 bg-white z-20">
-      <div className="container mx-auto flex items-center justify-between px-4 py-5 sm:p-6">
-        <div id="company-logo" className="relative size-8">
-          <a className="mx-2 my-1 flex items-center lg:mb-0 lg:mt-0" href="#">
-            <Image
-              src="https://tecdn.b-cdn.net/img/logo/te-transparent-noshadows.webp"
-              alt="TE Logo"
-              fill={true}
-              loading="lazy"
-            />
-          </a>
+    // <!-- ========== HEADER ========== -->
+    <header className="bg-white shadow shadow-black/30 dark:bg-neutral-800 dark:border-neutral-700 flex flex-wrap md:justify-start md:flex-nowrap sticky top-0 z-50 w-full p-4">
+      <nav className="relative max-w-[85rem] w-full md:flex md:items-center md:justify-between md:gap-3 mx-auto px-4 sm:px-6 lg:px-8 py-2">
+        {/* Logo w/ Collapse Button */}
+        <div className="flex items-center justify-between">
+          <Link
+            className="flex-none font-semibold text-xl text-black focus:outline-none focus:opacity-80 dark:text-white"
+            href="#"
+            aria-label="Brand"
+          >
+            Brand
+          </Link>
+          {/* Collapse Button */}
+          <div className="md:hidden">
+            <button
+              type="button"
+              className="hs-collapse-toggle relative size-9 flex justify-center items-center text-sm font-semibold rounded-lg border border-gray-200 text-black hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:border-neutral-700 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
+              id="hs-header-classic-collapse"
+              aria-expanded="false"
+              aria-controls="hs-header-classic"
+              aria-label="Toggle navigation"
+              data-hs-collapse="#hs-header-classic"
+            >
+              <svg
+                className="hs-collapse-open:hidden size-4"
+                xmlns="http://www.w3.org/2000/svg"
+                width={24}
+                height={24}
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth={2}
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <line x1={3} x2={21} y1={6} y2={6} />
+                <line x1={3} x2={21} y1={12} y2={12} />
+                <line x1={3} x2={21} y1={18} y2={18} />
+              </svg>
+              <svg
+                className="hs-collapse-open:block shrink-0 hidden size-4"
+                xmlns="http://www.w3.org/2000/svg"
+                width={24}
+                height={24}
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth={2}
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M18 6 6 18" />
+                <path d="m6 6 12 12" />
+              </svg>
+              <span className="sr-only">Toggle navigation</span>
+            </button>
+          </div>
+          {/* End Collapse Button */}
         </div>
-        <nav id="webpages" className="">
-          <ul className="flex items-center justify-end list-none">
-            <li>
-              <Link href="/">
-                <Button variant="link" size="sm">
-                  Home
-                </Button>
+        {/* End Logo w/ Collapse Button */}
+        {/* Collapse */}
+        <div
+          id="hs-header-classic"
+          className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow md:block"
+          aria-labelledby="hs-header-classic-collapse"
+        >
+          <div className="overflow-hidden overflow-y-auto max-h-[75vh] [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500">
+            <div className="py-2 md:py-0 flex flex-col md:flex-row md:items-center md:justify-end gap-0.5 md:gap-1">
+              <Link
+                className="p-2 flex items-center text-sm text-black hover:underline focus:text-black/60 dark:text-neutral-200 dark:hover:text-neutral-500 dark:focus:text-neutral-500"
+                href="#"
+                aria-current="page"
+              >
+                <svg
+                  className="shrink-0 size-4 me-3 md:me-2 block md:hidden"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={24}
+                  height={24}
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" />
+                  <path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+                </svg>
+                Home
               </Link>
-            </li>
-            <li>
-              <Link href="/about">
-                <Button variant="link" size="sm">
-                  About
-                </Button>
+              <Link
+                className="p-2 flex items-center text-sm text-black hover:underline focus:outline-none focus:text-black/60 dark:text-neutral-200 dark:hover:text-neutral-500 dark:focus:text-neutral-500"
+                href="#"
+              >
+                <svg
+                  className="shrink-0 size-4 me-3 md:me-2 block md:hidden"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={24}
+                  height={24}
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
+                  <circle cx={12} cy={7} r={4} />
+                </svg>
+                About
               </Link>
-            </li>
-            <li>
-              <Link href="/real-estate-process">
-                <Button variant="link" size="sm">
-                  Real-Estate Process
-                </Button>
+              <Link
+                className="p-2 flex items-center text-sm text-black hover:underline focus:outline-none focus:text-black/60 dark:text-neutral-200 dark:hover:text-neutral-500 dark:focus:text-neutral-500"
+                href="#"
+              >
+                <svg
+                  className="shrink-0 size-4 me-3 md:me-2 block md:hidden"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={24}
+                  height={24}
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="M12 12h.01" />
+                  <path d="M16 6V4a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v2" />
+                  <path d="M22 13a18.15 18.15 0 0 1-20 0" />
+                  <rect width={20} height={14} x={2} y={6} rx={2} />
+                </svg>
+                Real-Estate Transaction
               </Link>
-            </li>
-            <li>
-              <Link href="/listings">
-                <Button variant="link" size="sm">
-                  Listings
-                </Button>
+              <Link
+                className="p-2 flex items-center text-sm text-black hover:underline focus:outline-none focus:text-black/60 dark:text-neutral-200 dark:hover:text-neutral-500 dark:focus:text-neutral-500"
+                href="#"
+              >
+                <svg
+                  className="shrink-0 size-4 me-3 md:me-2 block md:hidden"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={24}
+                  height={24}
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="M4 22h16a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H8a2 2 0 0 0-2 2v16a2 2 0 0 1-2 2Zm0 0a2 2 0 0 1-2-2v-9c0-1.1.9-2 2-2h2" />
+                  <path d="M18 14h-8" />
+                  <path d="M15 18h-5" />
+                  <path d="M10 6h8v4h-8V6Z" />
+                </svg>
+                Contact
               </Link>
-            </li>
-            <li>
-              <Link href="/contact">
-                <Button variant="link" size="sm">
-                  Contact
-                </Button>
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
+              {/* End Button Group */}
+            </div>
+          </div>
+        </div>
+        {/* End Collapse */}
+      </nav>
     </header>
+
+    // <!-- ========== END HEADER ========== -->
   );
 }
