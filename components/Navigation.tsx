a8f9cf44442bf034a2095f5fbf4654118de5606a
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-08-08 11:27:13 -0400

    Replaced SVG's with React-Icon Syntax.

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,6 +1,6 @@
-import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
+import { AiOutlineMenu, AiOutlineHome, AiOutlineUser, AiOutlineTags, AiOutlineMail } from "react-icons/ai";
 
 export default function Navigation() {
   return (
@@ -27,37 +27,7 @@ export default function Navigation() {
               aria-label="Toggle navigation"
               data-hs-collapse="#hs-header-classic"
             >
-              <svg
-                className="hs-collapse-open:hidden size-4"
-                xmlns="http://www.w3.org/2000/svg"
-                width={24}
-                height={24}
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth={2}
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <line x1={3} x2={21} y1={6} y2={6} />
-                <line x1={3} x2={21} y1={12} y2={12} />
-                <line x1={3} x2={21} y1={18} y2={18} />
-              </svg>
-              <svg
-                className="hs-collapse-open:block shrink-0 hidden size-4"
-                xmlns="http://www.w3.org/2000/svg"
-                width={24}
-                height={24}
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth={2}
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path d="M18 6 6 18" />
-                <path d="m6 6 12 12" />
-              </svg>
+              <AiOutlineMenu className="hs-collapse-open:hidden size-4" />
               <span className="sr-only">Toggle navigation</span>
             </button>
           </div>
@@ -77,88 +47,28 @@ export default function Navigation() {
                 href="/"
                 aria-current="page"
               >
-                <svg
-                  className="shrink-0 size-4 me-3 md:me-2 block md:hidden"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width={24}
-                  height={24}
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" />
-                  <path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
-                </svg>
+                <AiOutlineHome className="shrink-0 size-4 me-3 md:me-2 block md:hidden" />
                 Home
               </Link>
               <Link
                 className="p-2 flex items-center text-sm text-black hover:underline focus:outline-none focus:text-black/60 dark:text-neutral-200 dark:hover:text-neutral-500 dark:focus:text-neutral-500"
                 href="/about"
               >
-                <svg
-                  className="shrink-0 size-4 me-3 md:me-2 block md:hidden"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width={24}
-                  height={24}
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
-                  <circle cx={12} cy={7} r={4} />
-                </svg>
+                <AiOutlineUser className="shrink-0 size-4 me-3 md:me-2 block md:hidden" />
                 About
               </Link>
               <Link
                 className="p-2 flex items-center text-sm text-black hover:underline focus:outline-none focus:text-black/60 dark:text-neutral-200 dark:hover:text-neutral-500 dark:focus:text-neutral-500"
                 href="/real-estate-process"
               >
-                <svg
-                  className="shrink-0 size-4 me-3 md:me-2 block md:hidden"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width={24}
-                  height={24}
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path d="M12 12h.01" />
-                  <path d="M16 6V4a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v2" />
-                  <path d="M22 13a18.15 18.15 0 0 1-20 0" />
-                  <rect width={20} height={14} x={2} y={6} rx={2} />
-                </svg>
+                <AiOutlineTags className="shrink-0 size-4 me-3 md:me-2 block md:hidden" />
                 Real-Estate Transaction
               </Link>
               <Link
                 className="p-2 flex items-center text-sm text-black hover:underline focus:outline-none focus:text-black/60 dark:text-neutral-200 dark:hover:text-neutral-500 dark:focus:text-neutral-500"
                 href="/contact"
               >
-                <svg
-                  className="shrink-0 size-4 me-3 md:me-2 block md:hidden"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width={24}
-                  height={24}
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path d="M4 22h16a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H8a2 2 0 0 0-2 2v16a2 2 0 0 1-2 2Zm0 0a2 2 0 0 1-2-2v-9c0-1.1.9-2 2-2h2" />
-                  <path d="M18 14h-8" />
-                  <path d="M15 18h-5" />
-                  <path d="M10 6h8v4h-8V6Z" />
-                </svg>
+                <AiOutlineMail className="shrink-0 size-4 me-3 md:me-2 block md:hidden" />
                 Contact
               </Link>
               {/* End Button Group */}
@@ -168,7 +78,5 @@ export default function Navigation() {
         {/* End Collapse */}
       </nav>
     </header>
-
-    // <!-- ========== END HEADER ========== -->
   );
 }
