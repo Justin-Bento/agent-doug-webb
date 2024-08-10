b13ea57edcf2456975356ae0a3eabf94b0f57ade
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-08-09 22:15:53 -0400

    Fixed nagiation clutter and responsive nature.

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,89 +1,66 @@
 "use client"
-import React, { useState } from "react";
-import { Button } from "@/components/ui/button";
-import { RiMenuLine, RiCloseLine } from "react-icons/ri";
-import Link from "next/link";
+
+import Link from "next/link"
+import { Button } from "./ui/button"
+import { useState } from "react"
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai"
 
 export default function Navigation() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(true);
-  const onClick = () => setMobileMenuOpen((mobileMenuOpen) => !mobileMenuOpen);
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen((isOpen) => !isOpen);
   return (
-    <header className="bg-white dark:bg-black sticky top-0 z-10  py-5 shadow dark:shadow-white/40">
-      <div className="px-6 grid grid-cols-1 justify-evenly lg:container lg:mx-auto lg:items-center md:grid-cols-2">
-        <div className="flex items-center justify-between gap-1">
-          <Link
-            className="flex-none font-semibold text-xl text-black hover:underline focus:outline-none focus:opacity-80 dark:text-white"
-            href="/"
-            aria-label="Brand"
-          >
-            Brand
-          </Link>
-          {mobileMenuOpen ? (
-            <Button
-              variant="outline"
-              size="icon"
-              className="lg:hidden dark:text-white"
-              onClick={onClick}
-              aria-label="nav-button-open"
-            >
-              <RiMenuLine />
-            </Button>
-          ) : (
-            <Button
-              variant="outline"
-              size="icon"
-              className="lg:hidden dark:text-white"
-              onClick={onClick}
-              aria-label="nav-button-close"
-            >
-              <RiCloseLine />
-            </Button>
-          )}
-        </div>
-        <div
-          className={`${mobileMenuOpen ? "hidden lg:flex lg:items-center" : ""
-            }`}
-        >
-          <nav className="flex flex-col justify-start w-full lg:justify-end lg:flex-row">
-            <Link href="/">
+    <>
+      <header className="relative z-20 bg-white shadow shadow-dark dark:bg-dark dark:shadow-white py-4">
+        <section className="grid grid-cols-1 lg:grid-cols-2 container mx-auto sm:px-6 lg:px-8 py-2">
+          <div className="flex items-center justify-between">
+            <Link href="/" className="flex-none font-semibold text-xl text-black focus:outline-none focus:opacity-80 dark:text-white">Brand</Link>
+            {isOpen ? (
               <Button
-                variant="link"
-                aria-label="navigation-link-home"
-                className="w-full"
-              >
-                Home
-              </Button>
-            </Link>
-            <Link href="/about">
+                variant="outline"
+                className="block lg:hidden ml-auto border-black dark:border-white"
+                onClick={toggle}
+                aria-label="Toggle navigation"
+              ><AiOutlineClose /></Button>
+            ) : (
               <Button
-                variant="link"
-                aria-label="navigation-link-about"
-                className="w-full"
-              >
-                About
-              </Button>
-            </Link>
-            <Link href="/real-estate-process">
-              <Button
-                variant="link"
-                aria-label="navigation-link-real-estate-process"
-                className="w-full"
-              >
-                Real-Estate Transaction 
-              </Button>
-            </Link>
-            <Link href="/contact">
-              <Button
-                variant="link"
-                aria-label="navigation-link-contact"
-                className="w-full"
-              >
-                Contact
-              </Button>
-            </Link>
+                variant="outline"
+                className="block lg:hidden ml-auto border-black dark:border-white"
+                onClick={toggle}
+                aria-label="Toggle navigation"
+              ><AiOutlineMenu /></Button>
+            )}
+          </div>
+          <nav className={`lg:flex lg:items-center ${isOpen ? 'block' : 'hidden'}`}>
+            <ul className="lg:flex space-y-6 my-5 lg:my-0 lg:space-x-1 lg:space-y-0">
+              <li>
+                <Link href="/">
+                  <Button variant="link">Home</Button>
+                </Link>
+              </li>
+              <li>
+                <Link href="/about">
+                  <Button variant="link">About</Button>
+                </Link>
+              </li>
+              <li>
+                <Link href="/real-estate-process">
+                  <Button variant="link">Real-Estate Process</Button>
+                </Link>
+              </li>
+              <li>
+                <Link href="/listings">
+                  <Button variant="link">Listings</Button>
+                </Link>
+              </li>
+              <li>
+                <Link href="/contact ">
+                  <Button variant="link">Contact</Button>
+                </Link>
+              </li>
+            </ul>
           </nav>
-        </div>
-      </div>
-    </header>
-  );
+        </section>
+      </header>
+    </>
+  )
 }
