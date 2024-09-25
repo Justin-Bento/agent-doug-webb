b15a52bc041a371d223ad3d8dda29fd1e72a058a
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-09-25 13:01:48 -0400

    Changed aspect ratio based to video for single colum items and square to 3-column items.

--- app/page.tsx
+++ app/page.tsx
@@ -90,15 +90,15 @@ export default function page() {
           <div className="col-span-12  mb-6 mt-3 w-full max-w-[6rem] ring-1 ring-black"></div>
           {/* <!-- 3x3 Grid Section --> */}
           <ul role="list" className="col-span-12 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-            <li className="bg-black aspect-square">&nbsp;</li>
-            <li className="bg-black aspect-square">&nbsp;</li>
-            <li className="bg-black aspect-square">&nbsp;</li>
-            <li className="bg-black aspect-square">&nbsp;</li>
-            <li className="bg-black aspect-square">&nbsp;</li>
-            <li className="bg-black aspect-square">&nbsp;</li>
-            <li className="bg-black aspect-square">&nbsp;</li>
-            <li className="bg-black aspect-square">&nbsp;</li>
-            <li className="bg-black aspect-square">&nbsp;</li>
+            <li className="bg-black aspect-video sm:aspect-square">&nbsp;</li>
+            <li className="bg-black aspect-video sm:aspect-square">&nbsp;</li>
+            <li className="bg-black aspect-video sm:aspect-square">&nbsp;</li>
+            <li className="bg-black aspect-video sm:aspect-square">&nbsp;</li>
+            <li className="bg-black aspect-video sm:aspect-square">&nbsp;</li>
+            <li className="bg-black aspect-video sm:aspect-square">&nbsp;</li>
+            <li className="bg-black aspect-video sm:aspect-square">&nbsp;</li>
+            <li className="bg-black aspect-video sm:aspect-square">&nbsp;</li>
+            <li className="bg-black aspect-video sm:aspect-square">&nbsp;</li>
           </ul>
           {/* <!-- Loader --> */}
           <div className="col-span-12 mt-8">
