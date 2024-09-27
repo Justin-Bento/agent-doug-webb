1eddab9dcc90f97ee4f789306a669d3ebd42a677
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-09-27 13:01:52 -0400

    Updated typography components

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -2,32 +2,24 @@ import type { MDXComponents } from "mdx/types";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
-    h1: ({ children }) => (
-      <h1 className="text-4xl tracking-tight font-extrabold capitalize dark:text-white">{children}</h1>
+    h1: ({ children }) => <h1 className="text-black dark:text-white">{children}</h1>,
+    h2: ({ children }) => <h2 className="text-black dark:text-white">{children}</h2>,
+    h3: ({ children }) => <h3 className="text-black dark:text-white">{children}</h3>,
+    h4: ({ children }) => <h4 className="text-black dark:text-white">{children}</h4>,
+    h5: ({ children }) => <h5 className="text-black dark:text-white">{children}</h5>,
+    h6: ({ children }) => <h6 className="text-black dark:text-white">{children}</h6>,
+    p: ({ children }) => (
+      <p className="text-base/8 tracking-wide font-sans text-balance text-black dark:text-white">{children}</p>
     ),
-    h2: ({ children }) => (
-      <h2 className="text-3xl tracking-tight font-extrabold first:mt-0 dark:text-white">{children}</h2>
+    small: ({ children }) => <small className="text-sm font-medium leading-none">{children}</small>,
+    strong: ({ children }) => (
+      <strong className="block my-5 text-lg font-semibold text-black dark:text-white">{children}</strong>
     ),
-    h3: ({ children }) => (
-      <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight dark:text-white">{children}</h3>
-    ),
-    h4: ({ children }) => (
-      <h4 className="scroll-m-20 text-xl font-semibold tracking-tight dark:text-white">{children}</h4>
-    ),
-    h5: ({ children }) => <h5 className="text-xl text-muted-foreground ">{children}</h5>,
-    h6: ({ children }) => (
-      <h6 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl dark:text-white">{children}</h6>
-    ),
-    p: ({ children }) => <p className="leading-7 dark:text-white">{children}</p>,
-    ul: ({ children }) => (
-      <ul className="list-disc list-inside text-black my-6 ml-6 [&>li]:mt-2 dark:text-white [&>li]:dark:text-white/90">
-        {children}
-      </ul>
-    ),
-    a: ({ children }) => <a className="underline text-primary dark:text-primary">{children}</a>,
-    blockquote: ({ children }) => (
-      <blockquote className="mt-6 border-l-2 pl-6 italic dark:text-white">{children}</blockquote>
-    ),
+    ul: ({ children }) => <ul className="list-disc my-5 list-outside text-black dark:text-white">{children}</ul>,
+    ol: ({ children }) => <ol className="list-decimal my-5 list-outside text-black dark:text-white">{children}</ol>,
+    li: ({ children }) => <li className="my-5 text-black dark:text-white">{children}</li>,
+    a: ({ children }) => <a className="text-black underline dark:text-white">{children}</a>,
+    blockquote: ({ children }) => <blockquote className="block text-black dark:text-white">{children}</blockquote>,
     ...components,
   };
 }
