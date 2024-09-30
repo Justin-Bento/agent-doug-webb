95d7d442c17276e134b09925cbf7063c7f8f2dc6
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-09-30 14:41:34 -0400

    Created a createSlug utility that changes a string to lowercase and replaces a space to a string

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function createSlug(name:string) {
+  return name.toLocaleLowerCase().replace(/\s+/g, '-')
+}
