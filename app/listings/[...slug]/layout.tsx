320ef2874e9f47db98c1ed495ad21a3ad51a262d
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-10-02 09:34:17 -0400

    Created layout page header for slug.

--- app/listings/[...slug]/layout.tsx
+++ app/listings/[...slug]/layout.tsx
@@ -8,7 +8,14 @@ export default function PropertyListingLayout() {
       <body>
         <Navigation />
         <main className="grid md:grid-cols-12 gap-4">
-          <section className="ring ring-black md:col-span-12">&nbsp;</section>
+          <section className="flex items-center md:col-span-12 min-h-[20dvh]">
+            <div className="wrapper flex flex-col-reverse">
+              <h1 className="max-w-3xl text-balance text-4xl font-medium tracking-normal text-black dark:text-white">
+                Property Name
+              </h1>
+              <p className="text-xs uppercase tracking-wider">Listings</p>
+            </div>
+          </section>
           <section className="ring ring-black md:col-span-12">&nbsp;</section>
           <section className="ring ring-black md:col-span-12">&nbsp;</section>
           <section className="ring ring-black md:col-span-12">&nbsp;</section>
