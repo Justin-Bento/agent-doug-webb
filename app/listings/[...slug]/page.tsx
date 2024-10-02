9d8de1bbf6927ced76d27186d7d4ea5ee7ac2f8f
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-10-02 13:13:45 -0400

    Added toaster component to main page for real estate

--- app/listings/[...slug]/page.tsx
+++ app/listings/[...slug]/page.tsx
@@ -1,5 +1,11 @@
+import { Toaster } from "@/components/ui/toaster";
 import React from "react";
 
 export default function page() {
-  return <div>page</div>;
+  return (
+    <>
+      <div>page</div>
+      <Toaster />
+    </>
+  );
 }
