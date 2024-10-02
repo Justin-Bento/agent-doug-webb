bf8e16028e37a8f4ac5b08369325f31b6b67be43
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-10-02 14:15:36 -0400

    Added default layout to main segments

--- app/real-estate-process/deciding-to-buy/page.tsx
+++ app/real-estate-process/deciding-to-buy/page.tsx
@@ -1,5 +1,7 @@
 import Footer from "@/components/Footer";
 import Navigation from "@/components/Navigation";
+import { Card } from "@/components/ui/card";
+import Link from "next/link";
 import React from "react";
 
 export default function DecidingToBuy() {
@@ -7,9 +9,29 @@ export default function DecidingToBuy() {
     <>
       <Navigation />
       <main className="wrapper min-h-dvh my-20">
-        <h1>Deciding To Buy</h1>
+        <section className="">
+          <h1 className="">Deciding To Buy</h1>
+        </section>
+        <section className="flex flex-col gap-4 mt-8">
+          {steps.map((step, index) => {
+            return (
+              <>
+                <Link href={`/real-estate-process/deciding-to-buy/${step}`}>
+                  <Card
+                    key={index}
+                    className="bg-transparent px-4 py-5 sm:p-4"
+                  >
+                    {step}
+                  </Card>
+                </Link>
+              </>
+            );
+          })}
+        </section>
       </main>
       <Footer />
     </>
   );
 }
+
+const steps = ["step1", "step2", "step3", "step4", "step5", "step6", "step7", "step8", "step9", "step10"];
