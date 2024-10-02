61cdfd5e3356ab5962d1ef73cdc22956bfe124ed
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-10-02 09:30:01 -0400

    Added a 12 columns to each section

--- app/listings/[...slug]/layout.tsx
+++ app/listings/[...slug]/layout.tsx
@@ -7,11 +7,11 @@ export default function PropertyListingLayout() {
     <html>
       <body>
         <Navigation />
-        <main className="grid md:grid-cols-12">
-          <section className="ring ring-black">&nbsp;</section>
-          <section className="ring ring-black">&nbsp;</section>
-          <section className="ring ring-black">&nbsp;</section>
-          <section className="ring ring-black">&nbsp;</section>
+        <main className="grid md:grid-cols-12 gap-4">
+          <section className="ring ring-black md:col-span-12">&nbsp;</section>
+          <section className="ring ring-black md:col-span-12">&nbsp;</section>
+          <section className="ring ring-black md:col-span-12">&nbsp;</section>
+          <section className="ring ring-black md:col-span-12">&nbsp;</section>
         </main>
         <Footer />
       </body>
