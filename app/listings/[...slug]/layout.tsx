88c5e31122a5d9fd2475c341e9415b8f8c34a188
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-10-02 09:03:19 -0400

    Created a dynamic layout that  renders real estate  properties as an individual lot.

---
+++ app/listings/[...slug]/layout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function PropertyListingLayout() {
+  return (
+    <html>
+      <body>
+        <h1 className="">layout</h1>
+      </body>
+    </html>
+  );
+}
