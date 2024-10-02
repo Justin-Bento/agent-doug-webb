bf2eadf93943ef2f44821771bf49f96173851525
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-10-02 11:53:41 -0400

    Added div  to main container instead of the whole page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -8,7 +8,7 @@ export default function page() {
   return (
     <>
       <Navigation />
-      <main className="grid grid-cols-1 md:grid-cols-12 gap-8">
+      <main className="">
         <section className="md:col-span-12 relative overflow-hidden">
           <BackgroundPattern className="w-full max-h-[20dvh] xl:max-h-72 z-10 bg-gradient-to-b from-accent/60 from-10% dark:from-accent/40 to-transparent" />
           <div className="absolute inset-0 z-20 flex flex-col items-start justify-center container mx-auto">
@@ -17,22 +17,24 @@ export default function page() {
             </h1>
           </div>
         </section>
-        <section className="w-full h-auto md:col-span-4 space-y-4 p-8">
-          <div className="">
-            <p className="font-semibold text-black dark:text-white">Douglas John Webb</p>
-            <p className="font-semibold text-black dark:text-white">Real Estate Representative</p>
-            <p className="font-semibold text-black dark:text-white">Right At Home Realty Inc.</p>
-          </div>
-          <div className="w-full h-[0.05rem] bg-black"></div>
-          <div className="">
-            <p className="font-medium text-black/50 dark:text-white">Direct: 416.845.9322</p>
-            <p className="font-medium text-black/50 dark:text-white">Office: 905.663.250</p>
-            <p className="font-medium text-black/50 dark:text-white">Email: abc123@gmail.com</p>
-          </div>
-        </section>
-        <section className="w-full h-auto md:col-span-8">
-          <ContactForm />
-        </section>
+        <div className="wrapper grid grid-cols-1 md:grid-cols-12 gap-8 my-12">
+          <section className="w-full h-auto md:col-span-4 space-y-4 p-8">
+            <div className="">
+              <p className="font-semibold text-black dark:text-white">Douglas John Webb</p>
+              <p className="font-semibold text-black dark:text-white">Real Estate Representative</p>
+              <p className="font-semibold text-black dark:text-white">Right At Home Realty Inc.</p>
+            </div>
+            <div className="w-full h-[0.05rem] bg-black"></div>
+            <div className="">
+              <p className="font-medium text-black/50 dark:text-white">Direct: 416.845.9322</p>
+              <p className="font-medium text-black/50 dark:text-white">Office: 905.663.250</p>
+              <p className="font-medium text-black/50 dark:text-white">Email: abc123@gmail.com</p>
+            </div>
+          </section>
+          <section className="md:col-span-7">
+            <ContactForm />
+          </section>
+        </div>
       </main>
       <Footer />
     </>
