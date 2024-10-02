a6b08af751a8f7c9e127a196c92421cb7a4aeee9
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-10-02 12:11:31 -0400

    Updated questions

--- app/real-estate-process/page.tsx
+++ app/real-estate-process/page.tsx
@@ -2,18 +2,8 @@ import React from "react";
 import Footer from "@/components/Footer";
 import Navigation from "@/components/Navigation";
 import Link from "next/link";
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-} from "@/components/ui/card";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
 import { Metadata } from "next";
 import { AiOutlineArrowRight } from "react-icons/ai";
 import BackgroundPattern from "@/components/BackgroundPattern";
@@ -33,23 +23,20 @@ export default function page() {
       <main className="min-h-dvh mb-28 space-y-28">
         <section className="relative overflow-hidden">
           <BackgroundPattern className="w-full max-h-[20dvh] xl:max-h-72 z-10 bg-gradient-to-b from-accent/60 from-10% dark:from-accent/40 to-transparent" />
-          <div className="absolute inset-0 z-20 flex flex-col items-start justify-center container mx-auto">
+          <div className="absolute inset-0 z-20 flex flex-col items-start justify-center wrapper">
             <h1 className="max-w-3xl text-pretty text-4xl font-medium tracking-tighter text-black dark:text-white sm:text-6xl">
               Real-Estate Process.
             </h1>
           </div>
         </section>
-        <section className="container mx-auto">
+        <section className="wrapper">
           <div className="grid lg:grid-cols-2 gap-8 mt-16">
             <Card className="bg-transparent dark:bg-[#16151b] dark:border-white/40">
-              <CardHeader className="text-lg font-bold">
-                Deciding To Buy.
-              </CardHeader>
+              <CardHeader className="text-lg font-bold">Deciding To Buy.</CardHeader>
               <CardContent>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Aperiam ipsa iste, recusandae itaque saepe iusto quae nemo
-                commodi voluptatem quam officia tempore! Neque atque architecto
-                saepe, autem eum ab perferendis!
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam ipsa iste, recusandae itaque saepe
+                iusto quae nemo commodi voluptatem quam officia tempore! Neque atque architecto saepe, autem eum ab
+                perferendis!
               </CardContent>
               <CardFooter>
                 <Link
@@ -62,14 +49,11 @@ export default function page() {
             </Card>
             {/* End of card */}
             <Card className="bg-transparent dark:bg-[#16151b] dark:border-white/40">
-              <CardHeader className="text-lg font-bold">
-                Deciding To Sell.
-              </CardHeader>
+              <CardHeader className="text-lg font-bold">Deciding To Sell.</CardHeader>
               <CardContent>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Aperiam ipsa iste, recusandae itaque saepe iusto quae nemo
-                commodi voluptatem quam officia tempore! Neque atque architecto
-                saepe, autem eum ab perferendis!
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam ipsa iste, recusandae itaque saepe
+                iusto quae nemo commodi voluptatem quam officia tempore! Neque atque architecto saepe, autem eum ab
+                perferendis!
               </CardContent>
               <CardFooter>
                 <Link
@@ -82,14 +66,11 @@ export default function page() {
             </Card>
             {/* End of card */}
             <Card className="bg-transparent dark:bg-[#16151b] dark:border-white/40">
-              <CardHeader className="text-lg font-bold">
-                Deciding To Sell.
-              </CardHeader>
+              <CardHeader className="text-lg font-bold">Deciding To Sell.</CardHeader>
               <CardContent>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Aperiam ipsa iste, recusandae itaque saepe iusto quae nemo
-                commodi voluptatem quam officia tempore! Neque atque architecto
-                saepe, autem eum ab perferendis!
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam ipsa iste, recusandae itaque saepe
+                iusto quae nemo commodi voluptatem quam officia tempore! Neque atque architecto saepe, autem eum ab
+                perferendis!
               </CardContent>
               <CardFooter>
                 <Link
@@ -102,14 +83,11 @@ export default function page() {
             </Card>
             {/* End of card */}
             <Card className="bg-transparent dark:bg-[#16151b] dark:border-white/40">
-              <CardHeader className="text-lg font-bold">
-                Additional Information.
-              </CardHeader>
+              <CardHeader className="text-lg font-bold">Additional Information.</CardHeader>
               <CardContent>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Aperiam ipsa iste, recusandae itaque saepe iusto quae nemo
-                commodi voluptatem quam officia tempore! Neque atque architecto
-                saepe, autem eum ab perferendis!
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam ipsa iste, recusandae itaque saepe
+                iusto quae nemo commodi voluptatem quam officia tempore! Neque atque architecto saepe, autem eum ab
+                perferendis!
               </CardContent>
               <CardFooter>
                 <Link
@@ -123,33 +101,43 @@ export default function page() {
             {/* End of card */}
           </div>
         </section>
-        <section className="container mx-auto">
-          <h2 className="text-4xl tracking-tight font-bold dark:text-white">
-            Frequently Asked Questions.
-          </h2>
-          <Accordion type="single" collapsible className="mt-4">
-            <AccordionItem value="item-1">
-              <AccordionTrigger>Is it accessible?</AccordionTrigger>
-              <AccordionContent>
-                Yes. It adheres to the WAI-ARIA design pattern.
-              </AccordionContent>
-            </AccordionItem>
-            <AccordionItem value="item-1">
-              <AccordionTrigger>Is it accessible?</AccordionTrigger>
-              <AccordionContent>
-                Yes. It adheres to the WAI-ARIA design pattern.
-              </AccordionContent>
-            </AccordionItem>
-            <AccordionItem value="item-1">
-              <AccordionTrigger>Is it accessible?</AccordionTrigger>
-              <AccordionContent>
-                Yes. It adheres to the WAI-ARIA design pattern.
-              </AccordionContent>
-            </AccordionItem>
-          </Accordion>
+        <section className="wrapper space-y-5">
+          <h2 className="text-4xl tracking-tight font-bold dark:text-white">Frequently Asked Questions.</h2>
+          {fakeDetails.map((detail, index) => {
+            return (
+              <Accordion
+                key={index}
+                type="single"
+                collapsible
+                className="w-full"
+              >
+                <AccordionItem value="item-1">
+                  <AccordionTrigger className="mt-1 text-lg/3 font-medium text-black dark:text-white">
+                    {detail}
+                  </AccordionTrigger>
+                  <AccordionContent>
+                    Jokester began sneaking into the castle in the middle of the night and leaving jokes all over the
+                    place: under the king&#39;s pillow, in his soup, even in the royal toilet. The king was furious, but
+                    he couldn&#39;t seem to stop Jokester. And then, one day, the people of the kingdom discovered that
+                    the jokes left by Jokester were so funny that they couldn&#39;t help but laugh. And once they
+                    started laughing, they couldn&#39;t stop.
+                  </AccordionContent>
+                </AccordionItem>
+              </Accordion>
+            );
+          })}
         </section>
       </main>
       <Footer />
     </>
   );
 }
+
+const fakeDetails = [
+  "What Is The Purpose Of This Guide?",
+  "Why Am I Sharing This Information?",
+  "Where Can We Contact Agent Webb?",
+  "When Will I Know I'm ready For A Property?",
+  "How Does This Help Me In My Search?",
+  //
+];
