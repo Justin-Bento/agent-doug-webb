8d2f419c5db9d4fcc6b8175ae15a80470f66a084
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-10-02 09:27:31 -0400

    Created base layout for every page.

--- app/listings/[...slug]/layout.tsx
+++ app/listings/[...slug]/layout.tsx
@@ -1,10 +1,16 @@
+import Footer from "@/components/Footer";
+import Navigation from "@/components/Navigation";
 import React from "react";
 
 export default function PropertyListingLayout() {
   return (
     <html>
       <body>
-        <h1 className="">layout</h1>
+        <Navigation />
+        <main className="">
+          <h1 className="">layout</h1>
+        </main>
+        <Footer />
       </body>
     </html>
   );
