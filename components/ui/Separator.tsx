4f71847b8f799553f9d3a59c0ed24af4d5a8415a
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-10-03 20:50:11 -0400

    Created a Separator component to ensure elements are consistent throughout the interface.

---
+++ components/ui/Separator.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Separator() {
+  return (
+    <>
+      <span className="block w-20 h-[0.2rem] bg-black mt-6 rounded-full"></span>
+    </>
+  );
+}
