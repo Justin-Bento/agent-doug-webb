a3fa612cb2f9db4370ea1841e644d573b48800fd
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-10-09 21:31:54 -0400

    Fixed gradient on banner

--- app/real-estate-process/page.tsx
+++ app/real-estate-process/page.tsx
@@ -23,8 +23,8 @@ export default function page() {
       <main className="min-h-dvh mb-28 space-y-28">
         <section className="relative overflow-hidden">
           <BackgroundPattern className="w-full max-h-[20dvh] xl:max-h-72 z-10 bg-gradient-to-b from-accent/60 from-10% dark:from-accent/40 to-transparent" />
-          <div className="absolute inset-0 z-20 flex flex-col items-start justify-center wrapper">
-            <h1 className="max-w-3xl text-pretty text-4xl font-medium tracking-tighter text-black dark:text-white sm:text-6xl">
+          <div className="wrapper absolute inset-0 z-20 flex flex-col items-start justify-center">
+            <h1 className="max-w-3xl text-pretty text-4xl font-medium tracking-tighter text-gray-950 dark:text-white sm:text-6xl">
               Real-Estate Process.
             </h1>
           </div>
