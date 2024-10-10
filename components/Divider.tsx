932b1dc46c512aa9b058b4f5866b222a69033f55
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-10-09 21:33:10 -0400

    Added dark variant to divider

--- components/Divider.tsx
+++ components/Divider.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Divider({ className }: { className?: string }) {
-  return <span className={`block w-20 h-[0.2rem] bg-black rounded-full ${className}`}></span>;
+  return <span className={`block w-20 h-[0.2rem] bg-black dark:bg-white/80 rounded-full ${className}`}></span>;
 }
