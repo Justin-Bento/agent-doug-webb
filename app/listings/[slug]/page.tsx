02e59ee101d4236d532b850380dfbaa2b641c7cf
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-10-20 11:30:18 -0400

    Added old layout to main content

--- app/listings/[slug]/page.tsx
+++ app/listings/[slug]/page.tsx
@@ -1,9 +1,28 @@
-import NotFound from "@/components/NotFound";
 import React from "react";
+import Link from "next/link";
 import property from "@/lib/property.json";
-
+import Footer from "@/components/Footer";
+import Navigation from "@/components/Navigation";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+import { Button } from "@/components/ui/button";
+import NotFound from "@/components/NotFound";
 // [x] Grab the page id from the slug url.
 // [] If the url display anything other than an item 1 through 10  render the not found component.
+
+interface fakeDetailsInter {
+  id: number;
+  title: string;
+  description: string;
+}
+
+const fakeDetails = [
+  "Features",
+  "Interior",
+  "Location",
+  "Area & Lot",
+  "Financial",
+  //
+];
 
 export default function page({ params }: any) {
   // Ensure params.slug is a number
@@ -19,10 +38,94 @@ export default function page({ params }: any) {
   }
 
   return (
-    <>
-      <div>
-        <p className="">page {params.slug}</p>
-      </div>
-    </>
+    <html>
+      <body>
+        <Navigation />
+        <main className="md:grid md:grid-cols-12 space-y-14">
+          <section className="md:col-span-12">
+            <div className="wrapper min-h-[20dvh] flex flex-col-reverse justify-center">
+              <h1 className="max-w-3xl text-balance text-4xl font-medium tracking-normal text-black dark:text-white">
+                Property
+              </h1>
+              <p className="text-xs uppercase tracking-wider">Listings</p>
+            </div>
+          </section>
+          <section className="wrapper md:col-span-12 md:flex items-center justify-between gap-12 space-y-6">
+            <div className="bg-gray-400 aspect-video md:aspect-[16/12] rounded-xl md:w-[60rem]">&nbsp;</div>
+            <div className="w-full max-w-prose">
+              <p className="text-base tracking-wide leading-5 max-w-[100rem]">
+                Jokester began sneaking into the castle in the middle of the night and leaving jokes all over the place:
+                under the king&#39;s pillow, in his soup, even in the royal toilet. The king was furious, but he
+                couldn&#39;t seem to stop Jokester. And then, one day, the people of the kingdom discovered that the
+                jokes left by Jokester were so funny that they couldn&#39;t help but laugh. And once they started
+                laughing, they couldn&#39;t stop.
+              </p>
+              <Button size="sm" variant="ghost" className="underline mt-6 border-black/15">
+                See More
+              </Button>
+            </div>
+          </section>
+          <section className="wrapper md:col-span-12 md:flex md:flex-row-reverse items-center justify-between gap-12 space-y-6">
+            <div className="bg-gray-400 aspect-video md:aspect-[16/12] rounded-xl md:w-[60rem]">&nbsp;</div>
+            <div className="w-full max-w-prose">
+              {fakeDetails.map((detail, index) => {
+                return (
+                  <Accordion key={index} type="single" collapsible className="w-full">
+                    <AccordionItem value="item-1">
+                      <AccordionTrigger className="mt-1 text-2xl/8 font-medium text-black dark:text-white">
+                        {detail}
+                      </AccordionTrigger>
+                      <AccordionContent>
+                        Jokester began sneaking into the castle in the middle of the night and leaving jokes all over
+                        the place: under the king&#39;s pillow, in his soup, even in the royal toilet. The king was
+                        furious, but he couldn&#39;t seem to stop Jokester. And then, one day, the people of the kingdom
+                        discovered that the jokes left by Jokester were so funny that they couldn&#39;t help but laugh.
+                        And once they started laughing, they couldn&#39;t stop.
+                      </AccordionContent>
+                    </AccordionItem>
+                  </Accordion>
+                );
+              })}
+            </div>
+          </section>
+          <section className="wrapper md:col-span-12">
+            <h3 className="max-w-3xl text-balance text-4xl font-medium tracking-normal text-black dark:text-white">
+              Inside The Property
+            </h3>
+            <p className="text-base tracking-wide leading-5 max-w-[100ch] mt-4">
+              Jokester began sneaking into the castle in the middle of the night and leaving jokes all over the place:
+              under the king&#39;s pillow, in his soup, even in the royal toilet. The king was furious, but he
+              couldn&#39;t seem to stop Jokester. And then, one day, the people of the kingdom discovered that the jokes
+              left by Jokester were so funny that they couldn&#39;t help but laugh. And once they started laughing, they
+              couldn&#39;t stop.
+            </p>
+            <div className="border-y-2 mt-6 border-primary dark:border-accent border-dotted">
+              <div className="overflow-x-auto scroll-p-16 py-4 scroll-snap-x snap-mandatory">
+                <div className="inline-grid grid-flow-col gap-5 snap-x auto-cols-[minmax(20rem,1fr)]">
+                  {Array.from({ length: 10 }, (_, index) => {
+                    return (
+                      <div
+                        key={index}
+                        className="w-full aspect-square bg-gray-400 rounded-xl snap-center flex items-center justify-center"
+                      >
+                        {index + 1}
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+          </section>
+          <section className="wrapper flex justify-center col-span-12 pb-20">
+            <Link href="/listings">
+              <Button variant="outline" className="text-sm rounded-full px-5">
+                View More Listings
+              </Button>
+            </Link>
+          </section>
+        </main>
+        <Footer />
+      </body>
+    </html>
   );
 }
