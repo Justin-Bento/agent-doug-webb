07424ed84d4afb525a54b4a86a07a222721fd0e9
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-10-21 14:19:28 -0400

    Created a second section called to showcase the stages of the buying process

--- app/real-estate-process/deciding-to-buy/page.tsx
+++ app/real-estate-process/deciding-to-buy/page.tsx
@@ -1,6 +1,8 @@
 import BackgroundPattern from "@/components/BackgroundPattern";
+import Divider from "@/components/Divider";
 import Footer from "@/components/Footer";
 import Navigation from "@/components/Navigation";
+import { Card } from "@/components/ui/card";
 import Link from "next/link";
 import React from "react";
 
@@ -18,28 +20,13 @@ export default function page() {
           </div>
         </section>
         <section className="wrapper">
-          <ul className="list-disc ml-6 mt-2">
-            <li>
-              <Link href="/real-estate-process/deciding-to-buy/1" className="hover:underline">
-                Deciding To Buy Step #1
-              </Link>
-            </li>
-            <li>
-              <Link href="/real-estate-process/deciding-to-buy/2" className="hover:underline">
-                Deciding To Buy Step #2
-              </Link>
-            </li>
-            <li>
-              <Link href="/real-estate-process/deciding-to-buy/3" className="hover:underline">
-                Deciding To Buy Step #3
-              </Link>
-            </li>
-            <li>
-              <Link href="/real-estate-process/deciding-to-buy/4" className="hover:underline">
-                Deciding To Buy Step #4
-              </Link>
-            </li>
-          </ul>
+          <h2 className="text-4xl tracking-tight font-bold">Sages Of The Buying Process</h2>
+          <Divider className="rounded-full my-4" />
+          <Link href="/real-estate-process/deciding-to-buy/1" className="hover:underline">
+            <Card className="px-4 py-5 sm:p-6">
+              <h3 className="">Buy Before You Sell</h3>
+            </Card>
+          </Link>
         </section>
       </main>
       <Footer />
