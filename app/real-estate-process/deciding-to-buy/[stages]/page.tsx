377c04f1b57e05e850466430337c9ffe97af6355
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-10-21 17:18:54 -0400

    Utilized the revertSlug function to grab the title from the paramaters

--- app/real-estate-process/deciding-to-buy/[stages]/page.tsx
+++ app/real-estate-process/deciding-to-buy/[stages]/page.tsx
@@ -1,5 +1,6 @@
 import Footer from "@/components/Footer";
 import Navigation from "@/components/Navigation";
+import { revertSlug } from "@/lib/utils";
 import React from "react";
 
 export default function page({ params }: any) {
@@ -7,7 +8,7 @@ export default function page({ params }: any) {
     <>
       <Navigation />
       <main className="wrapper min-h-dvh my-24">
-        <h1 className="text-xl font-bold"> Hello, Deciding To Buy - Stage {params.stages}!</h1>
+        <h1 className="text-xl font-bold"> Hello, Deciding To Buy - {revertSlug(params.stages)}!</h1>
       </main>
       <Footer />
     </>
