be463ac2f84ef4c95c67d7c1baf82978bdb071ff
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-10-21 17:18:03 -0400

    Created a function that reverts the creatSlug function to add as main title

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,10 +1,18 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
-export function createSlug(name:string) {
-  return name.toLocaleLowerCase().replace(/\s+/g, '-')
+export function createSlug(name: string) {
+  return name.toLocaleLowerCase().replace(/\s+/g, "-");
+}
+
+export function revertSlug(slug: string) {
+  // Replace hyphens with spaces
+  const reverted = slug.replace(/-/g, " ");
+
+  // Capitalize the first letter of each word (Title Case)
+  return reverted.replace(/\b\w/g, (char) => char.toUpperCase());
 }
