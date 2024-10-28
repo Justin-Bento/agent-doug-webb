fbe39190c72fd1684f1b7ba3e4d216bcbd9ac36e
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-10-28 19:52:16 -0400

    Added new protcol to enable images to be rendered from sanity

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "**",
       },
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
     ],
   },
 };
