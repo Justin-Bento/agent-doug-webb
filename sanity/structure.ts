0a0bbd3e01c2a63dcc09d7021f0ba19385da5b41
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-10-28 17:47:33 -0400

    Updated the structure of sanity to include two new document types called realEstate Process and property listings

--- sanity/structure.ts
+++ sanity/structure.ts
@@ -1,15 +1,25 @@
-import type {StructureResolver} from 'sanity/structure'
+import type { StructureResolver } from "sanity/structure";
 
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
   S.list()
-    .title('Blog')
+    .title("Blog")
     .items([
-      S.documentTypeListItem('post').title('Posts'),
-      S.documentTypeListItem('category').title('Categories'),
-      S.documentTypeListItem('author').title('Authors'),
+      S.documentTypeListItem("post").title("Posts"),
+      S.documentTypeListItem("category").title("Categories"),
+      S.documentTypeListItem("author").title("Authors"),
+      S.documentTypeListItem("realEstateProcess").title("Real-Estate Process"),
+      S.documentTypeListItem("propertyListings").title("Property Listings"),
       S.divider(),
       ...S.documentTypeListItems().filter(
-        (item) => item.getId() && !['post', 'category', 'author'].includes(item.getId()!),
+        (item) =>
+          item.getId() &&
+          ![
+            "post",
+            "category",
+            "author",
+            "realEstateProcess",
+            "propertyListings",
+          ].includes(item.getId()!)
       ),
-    ])
+    ]);
