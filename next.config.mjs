f0223302019fa3f9ed9913fe7984d12291a31013
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-10-28 22:48:43 -0400

    Removed config

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,11 @@
-import createMDX from "@next/mdx";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configure `pageExtensions`` to include MDX files
-  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
-  transpilePackages: ["next-mdx-remote"],
-  // Optionally, add any other Next.js config below
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**",
+        hostname: "images.unsplash.com",
       },
       {
         protocol: "https",
@@ -20,13 +15,5 @@ const nextConfig = {
   },
 };
 
-const withMDX = createMDX({
-  // Add markdown plugins here, as desired
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
-  },
-});
-
 // Merge MDX config with Next.js config
-export default withMDX(nextConfig);
+export default nextConfig;
