f0ac25a18dfa91528b3579b2949630eddf385d15
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-11-25 18:06:58 -0500

    Created a new query for Selling process in the real estate transaction.

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -2,11 +2,19 @@
 
 import { defineQuery } from "next-sanity";
 
-export const RE_PROCESS_POSTS_QUERY = defineQuery(
-  `*[_type == "realEstateProcess"]{ _id, title, slug, categories }`
+export const RE_PROCESS_POSTS_QUERY = defineQuery(`*[_type == "realEstateProcess"]{ _id, title, slug, categories }`);
+
+export const RE_PROCESS_POSTS_QUERY_SELLING = defineQuery(
+  `*[_type == "realEstateProcess" && "selling" in categories[]->slug.current] {
+  _id,
+  title,
+  slug,
+  categories[] -> {
+    title,
+    slug
+  }`
 );
 
-export const RE_PROCESS_ARTICLE_QUERY =
-  defineQuery(`*[_type == "realEstateProcess" && slug.current == $slug][0]{
+export const RE_PROCESS_ARTICLE_QUERY = defineQuery(`*[_type == "realEstateProcess" && slug.current == $slug][0]{
   _id, title, body, mainImage
 }`);
