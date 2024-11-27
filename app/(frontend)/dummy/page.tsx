78616b69dc0fcd01fe33fbe9b606eff2f982d2e8
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-11-26 22:41:35 -0500

    Coppied real estate process content to dummy page

--- app/(frontend)/dummy/page.tsx
+++ app/(frontend)/dummy/page.tsx
@@ -1,28 +1,98 @@
-import Divider from "@/components/Divider";
 import Link from "next/link";
 import React from "react";
-const realEstateProcess = ["Deciding To Buy", "Deciding To Sell", "More Stuff", "Additional Information"];
+import Navigation from "@/components/Navigation";
+import Footer from "@/components/Footer";
+import BackgroundPattern from "@/components/BackgroundPattern";
+import { Card } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import Divider from "@/components/Divider";
 
-function formatToSlug(slug: string) {
-  return slug.toLowerCase().replace(/\s+/g, "-");
-}
-export default function DummyPage() {
+export default function page() {
   return (
-    <main className="space-y-4">
-      <h1 className="">Hello, Dummy Page.</h1>
-      <ul className="list-decimal ml-6">
-        {realEstateProcess.map((process) => {
-          return (
-            <li key={process}>
-              <Link href={`/dummy/${formatToSlug(process)}`}>{process}</Link>
-            </li>
-          );
-        })}
-      </ul>
-      <Divider />
-      <Link href="/" className="block">
-        Back To Home page
-      </Link>
-    </main>
+    <>
+      <main className="space-y-24 mb-24 min-h-dvh">
+        <section className="relative overflow-hidden">
+          <BackgroundPattern className="w-full max-h-[20dvh] xl:max-h-72 z-10 bg-gradient-to-b from-accent/60 from-10% dark:from-accent/40 to-transparent" />
+          <div className="wrapper absolute inset-0 z-20 flex flex-col items-start justify-center">
+            <h1 className="max-w-3xl text-pretty text-4xl font-medium tracking-tighter text-gray-950 dark:text-white sm:text-6xl">
+              Real-Estate Process.
+            </h1>
+          </div>
+        </section>
+        <section className="wrapper">
+          <h2 className="max-w-3xl text-pretty text-4xl font-medium  text-gray-950 dark:text-white">
+            A Comprehensive Overview of the Real Estate Process: What You Need to Know
+          </h2>
+          <Divider className=" my-8" />
+          <div className="grid grid-cols-2 gap-8">
+            <Link href="/dummy/deciding-to-buy" className="last:hover:underline">
+              <Card className="bg-transparent hover:border-gray-400 active:ring-primary hover:bg-white/70 transition-all  ">
+                <div className="px-4 py-5 sm:p-6 space-y-2">
+                  <p className="text-xl font-semibold">Deciding To Buy</p>
+                  <p className="line-clamp-2">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore molestias voluptatem sint,
+                    expedita facilis enim. Velit rem officia fugiat libero eveniet animi nemo minus perspiciatis maxime
+                    nihil beatae, atque esse.
+                  </p>
+                  <Button variant="link" className="m-0 p-0">
+                    See More
+                  </Button>
+                </div>
+              </Card>
+            </Link>
+            <Link href="/dummy/deciding-to-sell" className="last:hover:underline">
+              <Card className="bg-transparent hover:border-gray-400 active:ring-primary hover:bg-white/70 transition-all  ">
+                <div className="px-4 py-5 sm:p-6 space-y-2">
+                  <p className="text-xl font-semibold">Deciding To Sell</p>
+                  <p className="line-clamp-2">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore molestias voluptatem sint,
+                    expedita facilis enim. Velit rem officia fugiat libero eveniet animi nemo minus perspiciatis maxime
+                    nihil beatae, atque esse.
+                  </p>
+                  <Button variant="link" className="m-0 p-0">
+                    See More
+                  </Button>
+                </div>
+              </Card>
+            </Link>
+            <Link href="/dummy/more-stuff" className="last:hover:underline">
+              <Card className="bg-transparent hover:border-gray-400 active:ring-primary hover:bg-white/70 transition-all  ">
+                <div className="px-4 py-5 sm:p-6 space-y-2">
+                  <p className="text-xl font-semibold">More Stuff</p>
+                  <p className="line-clamp-2">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore molestias voluptatem sint,
+                    expedita facilis enim. Velit rem officia fugiat libero eveniet animi nemo minus perspiciatis maxime
+                    nihil beatae, atque esse.
+                  </p>
+                  <Button variant="link" className="m-0 p-0">
+                    See More
+                  </Button>
+                </div>
+              </Card>
+            </Link>
+            <Link href="/dummy/additional-information" className="last:hover:underline">
+              <Card className="bg-transparent hover:border-gray-400 active:ring-primary hover:bg-white/70 transition-all  ">
+                <div className="px-4 py-5 sm:p-6 space-y-2">
+                  <p className="text-xl font-semibold">Additional Information</p>
+                  <p className="line-clamp-2">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore molestias voluptatem sint,
+                    expedita facilis enim. Velit rem officia fugiat libero eveniet animi nemo minus perspiciatis maxime
+                    nihil beatae, atque esse.
+                  </p>
+                  <Button variant="link" className="m-0 p-0">
+                    See More
+                  </Button>
+                </div>
+              </Card>
+            </Link>
+          </div>
+        </section>
+        <section className="wrapper">
+          <h2 className="max-w-3xl text-pretty text-4xl font-medium  text-gray-950 dark:text-white">
+            Frequently Asked Questions
+          </h2>
+        </section>
+      </main>
+    </>
   );
 }
