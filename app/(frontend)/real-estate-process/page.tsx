24f70b79e7b948006a81986e58ebacc3dadbd5a5
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-11-26 22:55:18 -0500

    Updated real estate process page to use nextjs catch all segments and sanity routing

--- app/(frontend)/real-estate-process/page.tsx
+++ app/(frontend)/real-estate-process/page.tsx
@@ -1,16 +1,15 @@
 import Link from "next/link";
 import React from "react";
-import Navigation from "@/components/Navigation";
-import Footer from "@/components/Footer";
 import BackgroundPattern from "@/components/BackgroundPattern";
 import { Card } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import Divider from "@/components/Divider";
+import { createSlug } from "@/lib/utils";
 
 export default function page() {
+  const realEstateProcess: string[] = ["deciding to buy", "deciding to sell", "More Stuff", "Additional Information"];
   return (
     <>
-      <Navigation />
       <main className="space-y-24 mb-24 min-h-dvh">
         <section className="relative overflow-hidden">
           <BackgroundPattern className="w-full max-h-[20dvh] xl:max-h-72 z-10 bg-gradient-to-b from-accent/60 from-10% dark:from-accent/40 to-transparent" />
@@ -22,90 +21,32 @@ export default function page() {
         </section>
         <section className="wrapper">
           <h2 className="max-w-3xl text-pretty text-4xl font-medium  text-gray-950 dark:text-white">
-            A Comprehensive Overview of the Real Estate Process: What You Need
-            to Know
+            A Comprehensive Overview of the Real Estate Process: What You Need to Know
           </h2>
           <Divider className=" my-8" />
-          <div className="grid grid-cols-2 gap-8">
-            <Link
-              href="/real-estate-process/deciding-to-buy"
-              className="last:hover:underline"
-            >
-              <Card className="bg-transparent hover:border-gray-400 active:ring-primary hover:bg-white/70 transition-all  ">
-                <div className="px-4 py-5 sm:p-6 space-y-2">
-                  <p className="text-xl font-semibold">Deciding To Buy</p>
-                  <p className="line-clamp-2">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Inventore molestias voluptatem sint, expedita facilis enim.
-                    Velit rem officia fugiat libero eveniet animi nemo minus
-                    perspiciatis maxime nihil beatae, atque esse.
-                  </p>
-                  <Button variant="link" className="m-0 p-0">
-                    See More
-                  </Button>
-                </div>
-              </Card>
-            </Link>
-            <Link
-              href="/real-estate-process/deciding-to-sell"
-              className="last:hover:underline"
-            >
-              <Card className="bg-transparent hover:border-gray-400 active:ring-primary hover:bg-white/70 transition-all  ">
-                <div className="px-4 py-5 sm:p-6 space-y-2">
-                  <p className="text-xl font-semibold">Deciding To Sell</p>
-                  <p className="line-clamp-2">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Inventore molestias voluptatem sint, expedita facilis enim.
-                    Velit rem officia fugiat libero eveniet animi nemo minus
-                    perspiciatis maxime nihil beatae, atque esse.
-                  </p>
-                  <Button variant="link" className="m-0 p-0">
-                    See More
-                  </Button>
-                </div>
-              </Card>
-            </Link>
-            <Link
-              href="/real-estate-process/more-stuff"
-              className="last:hover:underline"
-            >
-              <Card className="bg-transparent hover:border-gray-400 active:ring-primary hover:bg-white/70 transition-all  ">
-                <div className="px-4 py-5 sm:p-6 space-y-2">
-                  <p className="text-xl font-semibold">More Stuff</p>
-                  <p className="line-clamp-2">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Inventore molestias voluptatem sint, expedita facilis enim.
-                    Velit rem officia fugiat libero eveniet animi nemo minus
-                    perspiciatis maxime nihil beatae, atque esse.
-                  </p>
-                  <Button variant="link" className="m-0 p-0">
-                    See More
-                  </Button>
-                </div>
-              </Card>
-            </Link>
-            <Link
-              href="/real-estate-process/additional-information"
-              className="last:hover:underline"
-            >
-              <Card className="bg-transparent hover:border-gray-400 active:ring-primary hover:bg-white/70 transition-all  ">
-                <div className="px-4 py-5 sm:p-6 space-y-2">
-                  <p className="text-xl font-semibold">
-                    Additional Information
-                  </p>
-                  <p className="line-clamp-2">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Inventore molestias voluptatem sint, expedita facilis enim.
-                    Velit rem officia fugiat libero eveniet animi nemo minus
-                    perspiciatis maxime nihil beatae, atque esse.
-                  </p>
-                  <Button variant="link" className="m-0 p-0">
-                    See More
-                  </Button>
-                </div>
-              </Card>
-            </Link>
-          </div>
+          <ul className="grid grid-cols-1 md:grid-cols-2 gap-8">
+            {realEstateProcess.map((process, index) => {
+              return (
+                <li key={index} className="group">
+                  <Link href={`dummy/${createSlug(process)}`}>
+                    <Card className="bg-transparent  active:ring-primary hover:bg-white/70 hover:border-gray-400 transition-all  ">
+                      <div className="px-4 py-5 sm:p-6 space-y-2">
+                        <p className="text-xl font-semibold capitalize">{process}</p>
+                        <p className="line-clamp-2">
+                          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore molestias voluptatem sint,
+                          expedita facilis enim. Velit rem officia fugiat libero eveniet animi nemo minus perspiciatis
+                          maxime nihil beatae, atque esse.
+                        </p>
+                        <Button variant="link" className="m-0 p-0 group-hover:underline">
+                          See More
+                        </Button>
+                      </div>
+                    </Card>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
         </section>
         <section className="wrapper">
           <h2 className="max-w-3xl text-pretty text-4xl font-medium  text-gray-950 dark:text-white">
@@ -113,7 +54,6 @@ export default function page() {
           </h2>
         </section>
       </main>
-      <Footer />
     </>
   );
 }
