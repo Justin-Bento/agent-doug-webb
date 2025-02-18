2f80aab0f9daaeafc929cfceec19f11bdf8df43a
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-02-17 19:42:50 -0500

    Added Property demo function to hold unique props related to the property info

--- app/(frontend)/listings/[...slug]/layout.tsx
+++ app/(frontend)/listings/[...slug]/layout.tsx
@@ -2,6 +2,39 @@ import { sanityFetch } from "@/sanity/lib/live";
 import { PROPERTY_LISTINGS_POST_QUERY } from "@/sanity/lib/queries";
 import { notFound } from "next/navigation";
 import Link from "next/link";
+import Image from "next/image";
+import { urlFor } from "@/sanity/lib/image";
+import { Button } from "@/components/ui/button";
+
+interface PropertyDemoProps {
+  image: string;
+  altTitle: string;
+  statement: string;
+}
+const PropertyDemo: React.FC<PropertyDemoProps> = ({
+  image,
+  altTitle,
+  statement,
+}) => {
+  return (
+    <div className="overflow-hidden  grid grid-cols-1 md:grid-cols-2 gap-12">
+      <div className="relative aspect-[16/10] p-0 pb-6 overflow-hidden">
+        <Image
+          fill
+          src={urlFor(image)?.url() || ""}
+          alt={altTitle || "Property"}
+          className="object-cover object-center rounded-xl"
+        />
+      </div>
+      <div className="flex flex-col items-start justify-center">
+        <p className="">{statement}</p>
+        <Button variant="link" className="underline">
+          Read More
+        </Button>
+      </div>
+    </div>
+  );
+};
 
 export default async function Page({
   params,
@@ -22,6 +55,11 @@ export default async function Page({
       <section className="w-full min-h-[50dvh] flex items-center justify-center">
         <h1 className="text-4xl font-bold text-balance">{post?.title}</h1>
       </section>
+      <PropertyDemo
+        image={post?.mainImage}
+        altTitle={post.title}
+        statement={post.Statement}
+      />
       <section className="">
         <hr />
         <Link href="/posts">&larr; Return to index</Link>
