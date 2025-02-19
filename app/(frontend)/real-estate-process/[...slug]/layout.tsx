36ac9560cd8ee0b429455d6b614f24d2ad60cd6c
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-02-18 22:07:32 -0500

    Formatted document with prettier

--- app/(frontend)/real-estate-process/[...slug]/layout.tsx
+++ app/(frontend)/real-estate-process/[...slug]/layout.tsx
@@ -1,3 +1,11 @@
-export default function RealEstateProcessLayout({ children }: { children: React.ReactNode }) {
-  return <main className={`wrapper min-h-dvh grid items-center py-20 space-y-8`}>{children}</main>;
+export default function RealEstateProcessLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <main className={`wrapper min-h-dvh grid items-center py-20 space-y-8`}>
+      {children}
+    </main>
+  );
 }
