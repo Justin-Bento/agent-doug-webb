8eda6d46eb66c60b94d1f2b11225e505ee0ccb0a
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-02-21 18:05:46 -0500

    updated sanity live token to get realtime data changes.

--- sanity/lib/live.ts
+++ sanity/lib/live.ts
@@ -1,4 +1,14 @@
 import { client } from "@/sanity/lib/client";
 import { defineLive } from "next-sanity";
 
-export const { sanityFetch, SanityLive } = defineLive({ client });
+const token = process.env.NEXT_PUBLIC_SANITY_API_READ_TOKEN;
+
+if (!token) {
+  throw new Error("Missing NEXT_PUBLIC_SANITY_API_READ_TOKEN");
+}
+
+export const { sanityFetch, SanityLive } = defineLive({
+  client,
+  serverToken: token,
+  browserToken: token,
+});
