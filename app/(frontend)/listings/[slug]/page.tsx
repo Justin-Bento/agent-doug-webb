6bbae08de7da40828861cd7dbf9dd87eeeb2a13d
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-02-23 12:13:22 -0500

    Refactored code by deleting the  Square Feet function and moving jsx code into default function to resolve Vercel production issues and typescript type issues.

--- app/(frontend)/listings/[slug]/page.tsx
+++ app/(frontend)/listings/[slug]/page.tsx
@@ -116,48 +116,38 @@ export default async function Page({
           - This section includes:
             * Fake locations of the dummy places to showcase how the location is shown on the property.
       */}
-      <PropertySquareFeet
-        Indoor={post.listingInformation?.areaAndLot?.indoors}
-        Outdoor={post.listingInformation?.areaAndLot?.outdoors}
-      />
+      <section className="">
+        <div className="space-y-2">
+          <h5 className="max-w-3xl text-pretty text-4xl font-medium text-gray-950 dark:text-white">
+            Area & Lot
+          </h5>
+          <ul className="overflow-hidden flex flex-col md:flex-row md:space-evenly gap-12">
+            <li className="flex-1">
+              <Card className="bg-transparent px-4 py-5 sm:p-6 space-y-1">
+                <TbBoxModel className="size-10" />
+                <p className="text-xl font-medium">Indoors</p>
+                <p className="">
+                  {post.listingInformation?.areaAndLot?.indoors}
+                </p>
+              </Card>
+            </li>
+            <li className="flex-1">
+              <Card className="bg-transparent px-4 py-5 sm:p-6 space-y-1">
+                <TbBoxModel2 className="size-10" />
+                <p className="text-xl font-medium">Outdoors</p>
+                <p className="">
+                  {post.listingInformation?.areaAndLot?.outdoors}
+                </p>
+              </Card>
+            </li>
+          </ul>
+        </div>
+      </section>
       <PropertyFinancialInfo
         FinancialInformation={post.listingInformation?.financial}
       />
       <PropertyImage />
     </main>
-  );
-}
-
-interface PropertySquareFeetProps {
-  Indoor?: string;
-  Outdoor?: string;
-}
-
-function PropertySquareFeet({ Indoor, Outdoor }: PropertySquareFeetProps) {
-  return (
-    <section className="">
-      <div className="space-y-2">
-        <h5 className="max-w-3xl text-pretty text-4xl font-medium text-gray-950 dark:text-white">
-          Area & Lot
-        </h5>
-        <ul className="overflow-hidden flex flex-col md:flex-row md:space-evenly gap-12">
-          <li className="flex-1">
-            <Card className="bg-transparent px-4 py-5 sm:p-6 space-y-1">
-              <TbBoxModel className="size-10" />
-              <p className="text-xl font-medium">Indoors</p>
-              <p className="">{Indoor}</p>
-            </Card>
-          </li>
-          <li className="flex-1">
-            <Card className="bg-transparent px-4 py-5 sm:p-6 space-y-1">
-              <TbBoxModel2 className="size-10" />
-              <p className="text-xl font-medium">Outdoors</p>
-              <p className="">{Outdoor}</p>
-            </Card>
-          </li>
-        </ul>
-      </div>
-    </section>
   );
 }
 
