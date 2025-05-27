aaff6c2bf3a7f7621760edd9ea4c2b80cbcd636b
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-05-27 07:05:16 -0400

    Applied a link inside list items as these list items will take you further in the webpage.

--- app/(frontend)/real-estate-process/[...slug]/page.tsx
+++ app/(frontend)/real-estate-process/[...slug]/page.tsx
@@ -29,9 +29,15 @@ export default async function RealEstateProcessSlug({
             </Link>
           </div>
           <ul className="my-6 ml-6 list-disc [&>li]:mt-2">
-            <li>1st level of puns: 5 gold coins</li>
-            <li>2nd level of jokes: 10 gold coins</li>
-            <li>3rd level of one-liners : 20 gold coins</li>
+            <li>
+              <Link href="#">1st level of puns: 5 gold coins</Link>
+            </li>
+            <li>
+              <Link href="#">2nd level of jokes: 10 gold coins</Link>
+            </li>
+            <li>
+              <Link href="#">3rd level of one-liners : 20 gold coin</Link>
+            </li>
           </ul>
         </section>
       </main>
