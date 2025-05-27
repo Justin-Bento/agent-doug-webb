788e6487dc333795187d4d6b65341dec2f6ff1cb
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-05-27 08:11:43 -0400

    Created data file to store mapped objects and reduce code.

---
+++ lib/data.tsx
@@ -0,0 +1,38 @@
+export const listings = [
+  {
+    id: 1,
+    title: "Maple Ridge Estates",
+    alt: "filter description for an image",
+    media: "/media/img/b746b508b0c9405df110ebabbf09c983-cc_ft_960.jpg",
+  },
+  {
+    id: 2,
+    title: "Rocky Summit Retreat",
+    alt: "filter description for an image",
+    media: "/media/img/dillon-kydd-3Ignkeds3w8-unsplash.jpg",
+  },
+  {
+    id: 3,
+    title: "Cedarbrook Haven",
+    alt: "filter description for an image",
+    media: "/media/img/Investment_Opportunities.jpg",
+  },
+  {
+    id: 4,
+    title: "Aurora Shores Villas",
+    alt: "filter description for an image",
+    media: "/media/img/pexels-curtis-adams-1694007-4258275.jpg",
+  },
+  {
+    id: 5,
+    title: "Mapleleaf Manor",
+    alt: "filter description for an image",
+    media: "/media/img/dillon-kydd-3Ignkeds3w8-unsplash.jpg",
+  },
+  {
+    id: 6,
+    title: "Osprey Lake Cottages",
+    alt: "filter description for an image",
+    media: "/media/img/The-Neighbourhoods-of-Mount-Pleasant-Towns.jpg",
+  },
+];
