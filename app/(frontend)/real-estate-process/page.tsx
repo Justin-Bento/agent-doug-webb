914cd0db00d4569493efb4d499c896090a012bb6
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-05-27 08:24:52 -0400

    Created an unordered list with 4 list items that link to sub pages underneath real-estate-process rote

--- app/(frontend)/real-estate-process/page.tsx
+++ app/(frontend)/real-estate-process/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 export default function page() {
@@ -5,6 +6,22 @@ export default function page() {
     <>
       <main className="wrapper min-h-dvh my-24">
         <h1 className="">Hello, Real Estate Process Page.</h1>
+        <nav className="">
+          <ul className="list-disc list-inside ml-6 mt-6">
+            <li>
+              <Link href="#">Hello, Nested Page.</Link>
+            </li>
+            <li>
+              <Link href="#">Hello, Nested Page.</Link>
+            </li>
+            <li>
+              <Link href="#">Hello, Nested Page.</Link>
+            </li>
+            <li>
+              <Link href="#">Hello, Nested Page.</Link>
+            </li>
+          </ul>
+        </nav>
       </main>
     </>
   );
