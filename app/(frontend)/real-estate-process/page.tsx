d80a1407eace973d446b83f68989675cbcd55554
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-05-27 08:23:02 -0400

    Created a page for real estate process

---
+++ app/(frontend)/real-estate-process/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <>
+      <main className="wrapper min-h-dvh my-24">
+        <h1 className="">Hello, Real Estate Process Page.</h1>
+      </main>
+    </>
+  );
+}
