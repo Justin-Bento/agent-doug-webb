6ec7688a8229d013c4622672f438a33ccc412d72
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-05-27 07:12:03 -0400

    Created an array of 10 elements that can be safely mapped.

--- app/(frontend)/real-estate-process/[...slug]/page.tsx
+++ app/(frontend)/real-estate-process/[...slug]/page.tsx
@@ -29,30 +29,7 @@ export default async function RealEstateProcessSlug({
             </Link>
           </div>
           <ul className="my-6 ml-6 list-disc [&>li]:mt-2">
-            <li>
-              <Link
-                href="#"
-                className="hover:underline hover:underline-offset-2"
-              >
-                1st level of puns: 5 gold coins
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="#"
-                className="hover:underline hover:underline-offset-2"
-              >
-                2nd level of jokes: 10 gold coins
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="#"
-                className="hover:underline hover:underline-offset-2"
-              >
-                3rd level of one-liners : 20 gold coin
-              </Link>
-            </li>
+           {new Array(10).fill(null).map((_, index) => ())}
           </ul>
         </section>
       </main>
