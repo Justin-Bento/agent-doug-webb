1c13d18fb87354f47a0318cacb76b3552f7d5930
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-12-28 10:45:10 -0400

    Formatted document with prettier

--- app/(frontend)/about/page.tsx
+++ app/(frontend)/about/page.tsx
@@ -14,40 +14,55 @@ export default function about() {
   return (
     <>
       <main className="min-h-dvh mb-24 space-y-24">
-       <PageHeader title="Our strength is collaboration" />
+        <PageHeader title="Our strength is collaboration" />
         <section className="wrapper">
           <div className="mx-auto max-w-2xl lg:max-w-none">
             <div className="max-w-3xl *:text-xl/loose space-y-6 dark:text-white">
               <p>
-                We believe that our strength lies in our collaborative approach, which puts our clients at the center of
-                everything we do.
+                We believe that our strength lies in our collaborative approach,
+                which puts our clients at the center of everything we do.
               </p>
-                <p>
-                  Studio was started by three friends who noticed that developer studios were charging clients double
-                  what an in-house team would cost. Since the beginning, we have been committed to doing things
-                  differently by charging triple instead.
-                </p>
-                <p>
-                  At Studio, we&#39; re more than just colleagues — we&#39; re a family. This means we pay very little
-                  and expect people to work late. We want our employees to bring their whole selves to work. In return,
-                  we just ask that they keep themselves there until at least 6:30pm.
-                </p>
-              </div>
+              <p>
+                Studio was started by three friends who noticed that developer
+                studios were charging clients double what an in-house team would
+                cost. Since the beginning, we have been committed to doing
+                things differently by charging triple instead.
+              </p>
+              <p>
+                At Studio, we&#39; re more than just colleagues — we&#39; re a
+                family. This means we pay very little and expect people to work
+                late. We want our employees to bring their whole selves to work.
+                In return, we just ask that they keep themselves there until at
+                least 6:30pm.
+              </p>
+            </div>
           </div>
         </section>
         <section className="wrapper">
           <dl className="grid grid-cols-1 gap-10 sm:grid-cols-2 lg:auto-cols-fr lg:grid-flow-col lg:grid-cols-none items-center">
             <div className="card-container p-6 flex flex-col-reverse md:pl-8 relative before:absolute after:absolute before:bg-neutral-950 after:bg-neutral-950/10 before:left-0 before:top-0 before:h-6 before:w-px after:bottom-0 after:left-0 after:top-8 after:w-px">
-              <dt className="mt-2 text-base dark:text-white/70">Certified Real Estate investor</dt>
-              <dd className="font-display text-3xl font-semibold text-black sm:text-4xl dark:text-white">1985</dd>
+              <dt className="mt-2 text-base dark:text-white/70">
+                Certified Real Estate investor
+              </dt>
+              <dd className="font-display text-3xl font-semibold text-black sm:text-4xl dark:text-white">
+                1985
+              </dd>
             </div>
             <div className="card-container p-6 flex flex-col-reverse md:pl-8 relative before:absolute after:absolute before:bg-neutral-950 after:bg-neutral-950/10 before:left-0 before:top-0 before:h-6 before:w-px after:bottom-0 after:left-0 after:top-8 after:w-px">
-              <dt className="mt-2 text-base dark:text-white/70">Years of experience</dt>
-              <dd className="font-display text-3xl font-semibold text-black sm:text-4xl dark:text-white">25+</dd>
+              <dt className="mt-2 text-base dark:text-white/70">
+                Years of experience
+              </dt>
+              <dd className="font-display text-3xl font-semibold text-black sm:text-4xl dark:text-white">
+                25+
+              </dd>
             </div>
             <div className="card-container p-6 flex flex-col-reverse md:pl-8 relative before:absolute after:absolute before:bg-neutral-950 after:bg-neutral-950/10 before:left-0 before:top-0 before:h-6 before:w-px after:bottom-0 after:left-0 after:top-8 after:w-px">
-              <dt className="mt-2 text-base dark:text-white/70">Satisfied Clients and Referral</dt>
-              <dd className="font-display text-3xl font-semibold text-black sm:text-4xl dark:text-white">70%</dd>
+              <dt className="mt-2 text-base dark:text-white/70">
+                Satisfied Clients and Referral
+              </dt>
+              <dd className="font-display text-3xl font-semibold text-black sm:text-4xl dark:text-white">
+                70%
+              </dd>
             </div>
           </dl>
         </section>
@@ -68,17 +83,21 @@ export default function about() {
                 </h2>
                 <Divider className="" />
                 <p className="">
-                  Years ago, while working as sales associates at rival companies, Thomas, Ben, and Natalie were
-                  discussing a big client they had all been competing for. Joking about seeing the terms of each
-                  other&#39;s offers, they had an idea: what if they shared data to win deals and split the commission
-                  behind their companies&#39; backs? It turned out to be an incredible success, and that idea became the
-                  kernel for Radiant.
+                  Years ago, while working as sales associates at rival
+                  companies, Thomas, Ben, and Natalie were discussing a big
+                  client they had all been competing for. Joking about seeing
+                  the terms of each other&#39;s offers, they had an idea: what
+                  if they shared data to win deals and split the commission
+                  behind their companies&#39; backs? It turned out to be an
+                  incredible success, and that idea became the kernel for
+                  Radiant.
                 </p>
                 <p className="">
-                  Today, Radiant transforms revenue organizations by harnessing illegally acquired customer and
-                  competitor data, using it to provide extraordinary leverage. More than 30,000 companies rely on
-                  Radiant to undercut their competitors and extort their customers, all through a single integrated
-                  platform.
+                  Today, Radiant transforms revenue organizations by harnessing
+                  illegally acquired customer and competitor data, using it to
+                  provide extraordinary leverage. More than 30,000 companies
+                  rely on Radiant to undercut their competitors and extort their
+                  customers, all through a single integrated platform.
                 </p>
               </div>
             </div>
