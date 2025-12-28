1469fbcf5d316ade138c9675ba7a083f025be89e
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-12-28 10:04:14 -0400

    Updated layout to make a responsive grid.

--- app/(frontend)/contact/page.tsx
+++ app/(frontend)/contact/page.tsx
@@ -8,35 +8,23 @@ export default function page() {
     <>
       <main className="">
         <PageHeader title="Contact" />
-        <div className="wrapper grid grid-cols-1 md:grid-cols-12 gap-8 my-12">
-          <section className="w-full h-auto md:col-span-4 space-y-4 lg:p-8">
-            <div className="">
-              <p className="font-semibold text-black dark:text-white">
-                Douglas John Webb
-              </p>
-              <p className="font-semibold text-black dark:text-white">
-                Real Estate Representative
-              </p>
-              <p className="font-semibold text-black dark:text-white">
-                Right At Home Realty Inc.
-              </p>
+        <div className="wrapper grid grid-cols-1 md:grid-cols-3 items-st gap-8 my-12">
+          <section className="w-full h-auto md:col-span-1 space-y-4 lg:p-8">
+            <div className="[&>p]:font-semibold [&>p]:text-gray-950 [&>p]:dark:text-gray-50">
+              <p>Douglas John Webb</p>
+              <p>Real Estate Representative</p>
+              <p>Right At Home Realty Inc.</p>
             </div>
             <div className="">
               <Divider className="md:w-full" />
             </div>
-            <div className="">
-              <p className="font-medium text-black/50 dark:text-white">
-                Direct: 416.845.9322
-              </p>
-              <p className="font-medium text-black/50 dark:text-white">
-                Office: 905.663.250
-              </p>
-              <p className="font-medium text-black/50 dark:text-white">
-                Email: abc123@gmail.com
-              </p>
-            </div>
+            <ul className="[&>li]:font-medium [&>li]:text-gray-500 [&>li]:dark:text-gray-50">
+              <li>Direct: 416.845.9322</li>
+              <li>Office: 905.663.250</li>
+              <li>Email: abc123@gmail.com</li>
+            </ul>
           </section>
-          <section className="md:col-span-7">
+          <section className="md:col-span-2">
             <ContactForm />
           </section>
         </div>
