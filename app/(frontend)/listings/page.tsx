be0e6ec9d94fa5355c20cea88e2602154909fa04
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-12-29 09:35:33 -0400

    Removed duplicate semantics inside html code.

--- app/(frontend)/listings/page.tsx
+++ app/(frontend)/listings/page.tsx
@@ -10,35 +10,32 @@ export default function page() {
     <>
       <main className="min-h-dvh mb-24">
         <PageHeader title="Property Listings" />
-        <main className="min-h-dvh mb-24 space-y-12 lg:space-y-24">
-          <PageHeader title="Property Listings" />
-          <ul className="wrapper grid grid-cols-1 md:grid-cols-3 md:col-span-3 gap-6 lg:gap-12">
-            {listings.map((house: any) => {
-              return (
-                <li key={house.id}>
-                  <Link
-                    href={`/listings/${createSlug(house.title)}`}
-                    className="overflow-hidden sm:rounded-lg hover:underline hover:animate-pulse hover:opacity-80 hover:cursor-pointer"
-                  >
-                    <div className="relative aspect-video sm:aspect-square sm:rounded-lg">
-                      <Image
-                        fill
-                        src={house.media}
-                        alt={house.alt}
-                        className="object-cover object-center rounded-lg"
-                      />
-                    </div>
-                    <div className="mt-4">
-                      <p className="text-lg/8 font-medium tracking-normal text-black dark:text-white">
-                        {house.title}
-                      </p>
-                    </div>
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-        </main>
+        <ul className="wrapper grid grid-cols-1 md:grid-cols-3 md:col-span-3 gap-6 lg:gap-12">
+          {listings.map((house: any) => {
+            return (
+              <li key={house.id}>
+                <Link
+                  href={`/listings/${createSlug(house.title)}`}
+                  className="overflow-hidden sm:rounded-lg hover:underline hover:animate-pulse hover:opacity-80 hover:cursor-pointer"
+                >
+                  <div className="relative aspect-video sm:aspect-square sm:rounded-lg">
+                    <Image
+                      fill
+                      src={house.media}
+                      alt={house.alt}
+                      className="object-cover object-center rounded-lg"
+                    />
+                  </div>
+                  <div className="mt-4">
+                    <p className="text-lg/8 font-medium tracking-normal text-black dark:text-white">
+                      {house.title}
+                    </p>
+                  </div>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
       </main>
     </>
   );
