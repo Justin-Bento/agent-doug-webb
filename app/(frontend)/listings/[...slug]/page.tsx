1045c10bdb5ff7f7a8b2b9302c9e9d5dba062d1a
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-12-29 10:58:34 -0400

    Declared a dynamic route inside listings to showcase invidual properties provided by the agent.

---
+++ app/(frontend)/listings/[...slug]/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <main className=" min-h-dvh my-32">
+      <article className="wrapper">
+        <h1 className="display3">Hello World</h1>
+      </article>
+    </main>
+  );
+}
