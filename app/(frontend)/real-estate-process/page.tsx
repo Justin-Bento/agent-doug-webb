9ef2330402ec0f571f63f788a10f28b595fc0b60
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-12-29 14:00:22 -0400

    Removed header

--- app/(frontend)/real-estate-process/page.tsx
+++ app/(frontend)/real-estate-process/page.tsx
@@ -31,24 +31,8 @@ export default async function page() {
   return (
     <main className="min-h-dvh mb-24 space-y-12">
       <PageHeader title="Real Estate Process" />
-      <section className="wrapper grid grid-cols-2 gap-8">
-        <div className="col-span-2 [&>p]:text-lg [&>p]:max-w-[80ch] [&>p]:mb-3">
-          <h2 className="text-4xl/loose font-bold tracking-wide first:mt-0">
-            A Comprehensive Overview.
-          </h2>
-          <p className="text-xl max-w-5xl text-pretty tracking-wide">
-            As an Ontario Real Estate Expert with 20+ Years Experience.
-            I&apos;ve distilled two decades of property buying/selling
-            experience into 4 simple modules - complete with guides and tools to
-            help you acquire your perfect property.
-          </p>
-          <p>
-            Each module goes into a lot of detail about the process so take your
-            time reading this information!
-          </p>
-        </div>
-        <Divider />
-        <ul className="col-span-2 grid grid-cols-1 md:grid-cols-2 gap-6">
+      <section className="wrapper gap-8">
+        <ul className="grid grid-cols-1 md:grid-cols-2 gap-6">
           {posts.map((post: RealEstateModulesInterface) => (
             <li key={post._id} className="group relative isolate">
               <Link
@@ -62,7 +46,6 @@ export default async function page() {
                     <CardDescription className="mt-2 tracking-wide text-base/relaxed text-black">
                       {post.processCategoryDescription}
                     </CardDescription>
-             
                   </CardContent>
                 </Card>
               </Link>
@@ -70,20 +53,18 @@ export default async function page() {
           ))}
         </ul>
       </section>
-      <section className="wrapper mt-16">
-        <div className="flex flex-col-reverse">
-          <h2 className="text-4xl/loose font-bold tracking-wide first:mt-0">
+      <section className="wrapper grid grid-cols-1 gap-12">
+        <div className="col-span-1 space-y-3">
+          <h2 className="h1 max-w-3xl text-pretty">
             Things You Should Know Before Getting Started.
           </h2>
-          <p>Frequently Asked Questions!</p>
+          <p className="body-lead tracking-wide max-w-2xl text-pretty">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti
+            recusandae natus a cumque consequatur ipsum enim temporibus rem
+            veritatis. Laboriosam rerum veritatis esse minima quae nisi illo ?
+          </p>
         </div>
-        <p className="text-xl max-w-5xl text-pretty tracking-wide">
-          As an Ontario Real Estate Expert with 20+ Years Experience. I&apos;ve
-          distilled two decades of property buying/selling experience into 4
-          simple modules - here are some things I expect you to know before we
-          get started.
-        </p>
-        <Divider className="mt-6 mb-10" />
+        <div className="card-container">&nbsp;</div>
       </section>
     </main>
   );
