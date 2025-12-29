96715dcd6a5269aea9a8642f614dbcd528e7f8c7
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-12-29 09:35:10 -0400

    Diplicated listing sections from landing page to here a consistent interface.

--- app/(frontend)/listings/page.tsx
+++ app/(frontend)/listings/page.tsx
@@ -1,11 +1,44 @@
-import PageHeader from "@/components/PageHeader";
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import PageHeader from "@/components/PageHeader";
+import { listings } from "@/lib/data";
+import { createSlug } from "@/lib/utils";
 
 export default function page() {
   return (
     <>
       <main className="min-h-dvh mb-24">
         <PageHeader title="Property Listings" />
+        <main className="min-h-dvh mb-24 space-y-12 lg:space-y-24">
+          <PageHeader title="Property Listings" />
+          <ul className="wrapper grid grid-cols-1 md:grid-cols-3 md:col-span-3 gap-6 lg:gap-12">
+            {listings.map((house: any) => {
+              return (
+                <li key={house.id}>
+                  <Link
+                    href={`/listings/${createSlug(house.title)}`}
+                    className="overflow-hidden sm:rounded-lg hover:underline hover:animate-pulse hover:opacity-80 hover:cursor-pointer"
+                  >
+                    <div className="relative aspect-video sm:aspect-square sm:rounded-lg">
+                      <Image
+                        fill
+                        src={house.media}
+                        alt={house.alt}
+                        className="object-cover object-center rounded-lg"
+                      />
+                    </div>
+                    <div className="mt-4">
+                      <p className="text-lg/8 font-medium tracking-normal text-black dark:text-white">
+                        {house.title}
+                      </p>
+                    </div>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </main>
       </main>
     </>
   );
