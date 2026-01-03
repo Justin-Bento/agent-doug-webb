739080e477aa481fc9a07170500c4d38660d9147
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2026-01-03 11:21:45 -0400

    Updated peoperty page to include a fake listing with static data for layout purposes

--- app/(frontend)/listings/[...slug]/page.tsx
+++ app/(frontend)/listings/[...slug]/page.tsx
@@ -1,11 +1,92 @@
-import React from "react";
-
-export default function page() {
+import PageHeader from "@/components/PageHeader";
+// app/shop/[[...slug]]/page.tsx
+// Based on https://www.rightathomerealty.com/ON/toronto/m4w0b7/14380531-MLS-C12463497-na-1205---11-Yorkville-Avenue
+export default function ListingsPage({ params }: any) {
+  const { slug } = params;
+  const title = slug
+    .join("-")
+    .split("-")
+    .map((word: string) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+  // For /shop, slug will be undefined (or an empty array in some cases)
+  // For /shop/clothes, slug will be ['clothes']
+  // For /shop/clothes/tops, slug will be ['clothes', 'tops']
+  // app/shop/[[...slug]]/page.tsx
   return (
-    <main className=" min-h-dvh my-32">
-      <article className="wrapper">
-        <h1 className="display3">Hello World</h1>
-      </article>
-    </main>
+    <>
+      <main className="wrapper min-h-dvh my-24 grid grid-cols-2 gap-16">
+        {/* Row 1: Two columns */}
+        <section className="col-span-2 grid grid-cols-1 lg:grid-cols-2 gap-y-8 gap-x-16">
+          <div className="flex flex-col justify-center items-start gap-6">
+            <h1 className="scroll-m-20 text-4xl font-extrabold text-balance">
+              {title}
+            </h1>
+            <p className="body-lead tracking-wide leading-7 font-medium mt-4">
+              Welcome to 11 Yorkville Avenue, where sophistication, culture, and
+              convenience come together in one of Toronto's most prestigious
+              neighbourhoods. This beautifully designed and furnished
+              2-bedroom+den, 2-bath residence offers the perfect blend of modern
+              elegance and urban energy, surrounded by the very best the city
+              has to offer. Inside, sleek Miele appliances, floor-to-ceiling
+              windows, and high-end finishes create a space that feels
+              effortlessly refined.
+            </p>
+            <ul className="body-lead font-medium tracking-wide space-y-3 list-disc list-inside ml-8">
+              <li className="">Address: 1205 - 11 Yorkville Avenue</li>
+              <li className="">Country: Toronto, Ontario, Canada</li>
+              <li className="">Postal Code: M4W0B7</li>
+            </ul>
+            {/* Content */}
+          </div>
+          <div className="w-full aspect-square bg-black rounded-2xl">
+            {/* Media */}
+          </div>
+        </section>
+
+        <section className="col-span-2 [&>p]:text-lg [&>p]:leading-7 [&>p:not(:first-child)]:mt-6">
+          <h2 className="mt-10 scroll-m-20 border-b border-black pb-2 text-3xl font-semibold transition-colors first:mt-0">
+            Property Information
+          </h2>
+          <p>
+            Every detail speaks to comfort and quality, from the open-concept
+            layout to the natural light that fills the suite. Step outside and
+            you're in the heart of Yorkville home to designer boutiques along
+            the Mink Mile, five-star dining, and vibrant cafés that define
+            Toronto's luxury lifestyle.
+          </p>
+          <p>
+            Spend weekends exploring the Royal Ontario Museum, catching a
+            performance at the Royal Conservatory of Music, or unwinding in the
+            serene green spaces of nearby Rosedale. The University of Toronto,
+            Bay subway station, and the Financial District are all just moments
+            away, making this the ideal home for those who value both elegance
+            and accessibility.
+          </p>
+          <p>
+            Life at 11 Yorkville means more than just a beautiful suite, it's a
+            complete lifestyle. Residents enjoy resort-inspired amenities,
+            including a rooftop pool and spa, state-of-the-art fitness centre,
+            piano lounge, private dining rooms, and more. Whether you're
+            entertaining, relaxing, or recharging, this building offers the best
+            of everything right at home. Experience Torontos most sophisticated
+            address 11 Yorkville Avenue, where every detail embodies prestige,
+            convenience, and elevated living. ** Parking available at $350.00
+            per month.
+          </p>
+          <ul className="flex gap-6 *:text-center  *:border divide-x-2">
+            <li className="">2 Bedrooms</li>
+            <li className="">2 Bathrooms</li>
+            <li className="">800-899 square feet</li>
+            <li className="">80 days</li>
+          </ul>
+        </section>
+
+        <section className="col-span-2">
+          <h2 className="mt-10 scroll-m-20 border-b border-black pb-2 text-3xl font-semibold transition-colors first:mt-0">
+            Additional Information
+          </h2>
+        </section>
+      </main>
+    </>
   );
 }
